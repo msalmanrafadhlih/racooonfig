254e1267fa682a87e648fb8d6d391d3991db61f0
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-12 22:27:47 +0700

    BUG(determinate nixpkgs) : deleted nixpkgs from specialArgs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,63 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  }
-  // import ./inputs {};
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ## ---- System Packages
+    xytz = {
+      url = "github:TQ-See/xytz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rip = {
+      url = "github:TQ-See/process-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    st-nix = {
+      url = "github:TQ-See/st-flexipatch";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ## ---- Home-Manager Packages
+    zjstatus = {
+      url = "github:dj95/zjstatus";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    textfox = {
+      url = "github:adriankarlen/textfox";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    yazi-flavors = {
+      url = "github:yazi-rs/flavors";
+      flake = false;
+    };
+
+    yazi-compress = {
+      url = "github:KKV9/compress.yazi";
+      flake = false;
+    };
+  };
 
   outputs = { home-manager, ... }@inputs:
   let
@@ -16,7 +71,7 @@
   in
   {
     # a NixOS module that root flake can import in mkHost
-    nixosModules.default = { hostname, system, nixpkgs, ... }:
+    nixosModules.default = { hostname, system, ... }:
     {
       imports = [ home-manager.nixosModules.home-manager ];
       home-manager = {
@@ -24,7 +79,6 @@
         useUserPackages = true;
         extraSpecialArgs = { inherit inputs system hostname; };
         backupFileExtension = "backup";
-        pkgs = nixpkgs.legacyPackages.${system};
       };
     };
 
