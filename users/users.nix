d3c7656e6aded784c7a093c2c60d068297d6cc97
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-03-18 12:08:15 +0700

    feat(users): configure gamemode specialisation

--- users/users.nix
+++ users/users.nix
@@ -1,13 +1,42 @@
-{ username, lib, ... }:
+{ username, lib, pkgs, ... }:
 
 {
   home.username = lib.mkDefault username;
   home.homeDirectory = "/home/${username}";
   programs.git.enable = true;
 
+
+
+  # Then use with `home-manager switch --specialisation gamemode`
   specialisation.gamemode.configuration = {
-    home.username = lib.mkForce "gamemode";
-    home.homeDirectory = "/home/${username}";
-    programs.git.enable = true;
+  # Well, My laptop sekarang kentang! dan butuh minimal usage.
+  # Execute this cmd jika perangkat kalian support gaming
+  # or adds your gaming configurations here 'its up to you..'
+
+    programs.mangohud = {
+      enable = true;
+      enableSessionWide = true; 
+      settings = {
+        fps_limit = 144;
+        cpu_temp = true;
+        gpu_temp = true;
+        ram = true;
+        fps = true;
+        frame_timing = 1;
+      };
+    };
+
+    # # to use gamemode with steam edit launch options inside
+    # # game -> general -> launch options -> `gamemoderun %command%`
+    # steam.enable = true;
+    gamemode.enable = true;
+
+    home.packages = with pkgs; [
+      lutris
+      heroic
+      steam-run
+      winboat
+    ];
+
   };
 }
