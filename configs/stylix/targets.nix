1bd858b81050343ab66c1972843ff99bac296e01
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-13 23:16:35 +0700

    2026-04-13 23:16 | Update via SAVEFLAKE: configs/stylix/targets.nix

--- configs/stylix/targets.nix
+++ configs/stylix/targets.nix
@@ -1,10 +1,18 @@
+{ inputs, ... }:
+
 {
-  stylix.targets = {
-    spicetify.enable = true;
-    helix.enable = false;
-    zellij.enable = false;
-    starship.enable = false;
-    zed.enable = false;
-    tmux.enable = false;
+  imports = [
+    inputs.stylix.homeModules.stylix
+  ];
+
+  stylix = {
+    targets = {
+      spicetify.enable = true;
+      helix.enable = false;
+      zellij.enable = false;
+      starship.enable = false;
+      zed.enable = false;
+      tmux.enable = false;
+    };
   };
 }
