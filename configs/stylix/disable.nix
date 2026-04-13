fb155099907ea7535978021a31c825404e467ddd
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-13 18:55:11 +0700

    2026-04-13 18:55 | Update via SAVEFLAKE: configs/stylix/disable.nix

--- configs/stylix/disable.nix
+++
@@ -1,8 +0,0 @@
-{
-  # stylix.targets.spicetify.enable = false;
-  stylix.targets.helix.enable = false;
-  stylix.targets.zellij.enable = false;
-  stylix.targets.starship.enable = false;
-  stylix.targets.zed.enable = false;
-  stylix.targets.tmux.enable = false;
-}
