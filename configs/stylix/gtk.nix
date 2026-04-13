9d5f1c93dd68723846c478c73234b5e2d9cd4f25
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-13 23:00:06 +0700

    2026-04-13 23:00 | Update via SAVEFLAKE: configs/stylix/gtk.nix

--- configs/stylix/gtk.nix
+++ configs/stylix/gtk.nix
@@ -1,21 +1,26 @@
+{ username, ... }: let
+  home = "/home/${username}";
+in
 {
   stylix = {
     targets.gtk = {
       enable = true;
       colors = {
         ## Whether to enable & override `config.lib.stylix.colors`
-        enable = true;
+        enable = false;
         ## Attribute sets are recursively merged with `config.lib.stylix.colors`,
         ## while all other non-null types override `config.lib.stylix.colors`.
         # override = null;
       };
 
       ## Extra code added
-      # extraCss ='''';
+      extraCss =''
+          @import url("${home}/.config/gtk-3.0/colors.css");
+        '';
 
       ## Whether to enable theming for support for theming Flatpak apps.
       ## Default : config.stylix.autoEnable
-      flatpakSupport.enable = true;
+      # flatpakSupport.enable = true;
 
       ## Whether to enable & override config.stylix.fonts.
       fonts = {
