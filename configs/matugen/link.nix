dd1ae5f77afb975a452ea5f8590aa8d1f645e914
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-13 23:06:20 +0700

    2026-04-13 23:06 | Update via SAVEFLAKE

---
+++ configs/matugen/link.nix
@@ -0,0 +1,19 @@
+{ dotfiles, config, ... }:
+let
+  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  home = config.home.homeDirectory;
+  dotfiles_path = "${home}/.dotfiles/${dotfiles}/configs/matugen";
+
+  configs = {
+		templates = "templates";
+		websites = "websites";
+  };
+
+in
+{
+  # Symlink path to ~./config/*
+  xdg.configFile = builtins.mapAttrs (name: subpath: {source =
+    create_symlink "${dotfiles_path}/${subpath}";
+    recursive = true;
+  }) configs;
+}
