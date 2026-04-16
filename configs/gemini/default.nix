1424aa6b7f76c10b9454556e03a9e139c93fd22b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-16 09:39:42 +0700

    2026-04-16 09:39 | Update via SAVEFLAKE: configs/gemini/default.nix

--- configs/gemini/default.nix
+++ configs/gemini/default.nix
@@ -1,34 +1,14 @@
-{ pkgs, ... }: {
-  home.file.".gemini/settings.json".text = builtins.toJSON {
-    ui = {
-      hideTips = true;
-      hideBanner = true;
-      hideWindowTitle = true;
-      useFullWidth = true;
-      showMemoryUsage = true;
-      theme = "Default";
-
-      footer = {
-        hideCWD = true;
-        hideSandboxStatus = true;
-        hideContextPercentage = true;
-      };
-    };
-
-    context = { includeDirectories = [ "~/.gemini" ]; };
-
-    general = {
-      preferredEditor = "hx";
-      vimMode = true;
-      disableAutoUpdate = true;
-      disableUpdateNag = true;
-      enablePromptCompletion = true;
-    };
-
-    security = { auth = { selectedType = "oauth-personal"; }; };
-
-    privacy = { usageStatisticsEnabled = false; };
+{ pkgs, mkSymlink, ... }: let
+  configs = {
+    ".gemini/settings.json" = "./settings.json";
   };
+in {
 
-  home.packages = with pkgs; [ gemini-cli ];
+  home.file = mkSymlink {
+    target = "gemini";
+  } configs;
+
+  home.packages = with pkgs; [
+    gemini-cli
+  ];
 }
