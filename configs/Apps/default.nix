778c85da685da3cc6bf4573c8de57de5d56d5ca3
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-16 09:39:42 +0700

    2026-04-16 09:39 | Update via SAVEFLAKE: configs/Apps/default.nix

--- configs/Apps/default.nix
+++ configs/Apps/default.nix
@@ -1,19 +1,3 @@
 # ./modules/applications.nix
-{ config, dotfiles, ... }:
-
-let
-  home = config.home.homeDirectory;
-
-  dotfiles_path = "${home}/.dotfiles/${dotfiles}/configs";
-  create_symlink = path: config.lib.file.mkOutOfStoreSymlink path;
-
-  locals = import ./launchers.nix ;
-in
-{
-
-  # /home/user custom file
-  home.file = builtins.mapAttrs (name: subpath: {source =
-   create_symlink "${dotfiles_path}/${subpath}";
-   recursive = true;
-  }) locals;
-}
+{ mkSymlink, ... }: let locals = import ./launchers.nix ;
+in { home.file = mkSymlink {} locals; }
