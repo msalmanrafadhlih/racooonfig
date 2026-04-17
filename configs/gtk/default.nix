0bf942f5d9a2effbaa6ec7aa8b767e04ac703a1d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-17 08:40:53 +0700

    2026-04-17 08:40 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -19,24 +19,12 @@ in {
     gsettings-desktop-schemas
   ];
 
-  # Cursor fallback global
-  home.pointerCursor = {
-    name = "Kafka";
-    package = pkgs.cursor-memes;
-    size = 24;
-    gtk.enable = true;
-    x11.enable = true;
-  };
-
   # Home variables
   home.sessionVariables = {
     XCURSOR_THEME = "Kafka";
     XCURSOR_SIZE = "24";
 
     QT_QPA_PLATFORMTHEME = "qt5ct";
-
-    # GTK_THEME = "";
-    # QT_STYLE_OVERRIDE = "";
   };
 
   # ✅ Pastikan schema tersedia
