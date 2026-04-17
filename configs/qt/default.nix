f6aa4dda63c5814b12d32b6e0bf1f5846177d7dc
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-17 07:40:49 +0700

    2026-04-17 07:40 | Update via SAVEFLAKE

---
+++ configs/qt/default.nix
@@ -0,0 +1,13 @@
+{ mkSymlink, ... }: let
+
+  configs = {
+   "qt5ct/qt5ct.conf" = "qt5ct.conf";
+   "qt6ct/qt6ct.conf" = "qt6ct.conf";
+  };
+in {
+  
+  # QT config (biar ikut icon/theme)
+  xdg.configFile = mkSymlink {
+    target = "qt";
+  } configs;
+}
