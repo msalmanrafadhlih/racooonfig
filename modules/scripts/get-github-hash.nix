b2582f6208a102ce107eb941351460c15b96ae39
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-22 10:12:08 +0700

    2026-04-22 10:12 | Update via SAVEFLAKE

---
+++ modules/scripts/get-github-hash.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+let
+  get-github-hash = pkgs.writeShellApplication {
+    name = "get-github-hash";
+    runtimeInputs = [ pkgs.nix-prefetch-github ];
+    text = ''
+      read -r -p "Masukkan Owner: " owner
+      read -r -p "Masukkan Repo: " repo
+      read -r -p "Masukkan Rev (Commit/Tag/Branch): " rev
+
+      if [[ -z "$owner" || -z "$repo" || -z "$rev" ]]; then
+        echo "Error: Semua input wajib diisi!"
+        exit 1
+      fi
+
+      echo "Mengambil hash..."
+      nix-prefetch-github "$owner" "$repo" --rev "$rev"
+    '';
+  };
+in
+{
+  environment.systemPackages = [ get-github-hash ];
+}
