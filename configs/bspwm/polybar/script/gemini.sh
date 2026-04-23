92e99418b210bc530223b9374935de6dbd8bae6c
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-23 09:35:02 +0700

    2026-04-23 09:35 | Update via SAVEFLAKE: configs/bspwm/polybar/script/gemini.sh

--- configs/bspwm/polybar/script/gemini.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-#surf -K "https://gemini.google.com"
-
-kdocker -f -q chromium --user-data-dir=$HOME/.config/vivaldi/Chromium --app="https://gemini.google.com" --class=GeminiAi &
