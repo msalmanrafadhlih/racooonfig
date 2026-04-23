b726dd788f9813d6304f7f6031da4f8f9bd2aa8a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-24 00:19:30 +0700

    2026-04-24 00:19 | Update via SAVEFLAKE: configs/stylix/default.nix

--- configs/stylix/default.nix
+++ configs/stylix/default.nix
@@ -67,7 +67,7 @@ in
 
     imageScalingMode = "fill"; # stretch, fill, fit, center, tile
     image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/msalmanrafadhlih/Dotfiles/refs/heads/bspwm/home/Assets/Wallpaper/wallpaper8.jpeg";
+      url = "https://raw.githubusercontent.com/msalmanrafadhlih/racooonfig/refs/heads/main/configs/Assets/Wallpaper/red/wallpaper8.jpeg";
       hash = "sha256-VZp1wy2N0GApt48ILRY+pIAhAjCt02GmqmxHRTWAEoA=";
     };
     # image = pkgs.fetchurl {
