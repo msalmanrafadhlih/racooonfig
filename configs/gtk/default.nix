c28fdc4003c5b89f3857ceac4a6a300ef666e4a2
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-26 18:41:58 +0700

    2026-04-26 18:41 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -10,12 +10,12 @@ in {
   ];
 
   # Copy/symlink theme ke ~/.themes
-  home.file.".themes/dynamic/gtk-4.0/assets".source = ./gtk-4.0/assets;
-  home.file.".themes/dynamic/gtk-4.0/libadwaita.css".source = ./gtk-4.0/libadwaita.css;
-  home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source = ./gtk-4.0/libadwaita-tweaks.css;
+  home.file.".themes/dynamic/gtk-4.0/assets".source = ./.themes/gtk-4.0/assets;
+  home.file.".themes/dynamic/gtk-4.0/libadwaita.css".source = ./.themes/gtk-4.0/libadwaita.css;
+  home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source = ./.themes/gtk-4.0/libadwaita-tweaks.css;
 
-  home.file.".themes/dynamic/gtk-3.0".source = ./gtk-3.0;
-  home.file.".themes/dynamic/index.theme".source = ./index.theme;
+  home.file.".themes/dynamic/gtk-3.0".source = ./.themes/gtk-3.0;
+  home.file.".themes/dynamic/index.theme".source = ./.themes/index.theme;
 
   # gtk.css
   home.file.".themes/dynamic/gtk-4.0/gtk.css".text = ''
