8d6f6d43484f03ee1571d9ffb960ec471a59b33d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-26 19:39:46 +0700

    2026-04-26 19:39 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -3,8 +3,8 @@
 let
   HOME = config.home.homeDirectory;
   configs = {
-    "gtk.3-0/settings.ini" = "settings.ini";
-    "gtk.4-0/settings.ini" = "settings.ini";
+    "gtk-3.0/settings.ini" = "settings.ini";
+    "gtk-4.0/settings.ini" = "settings.ini";
   };
 in {
   home.packages = with pkgs; [
@@ -18,6 +18,8 @@ in {
   } configs;
 
   # Copy/symlink theme ke ~/.themes
+  home.file.".gtkrc-2.0".source = ./gtkrc-2.0;
+
   home.file.".themes/dynamic/gtk-4.0/assets".source = ./.themes/gtk-4.0/assets;
   home.file.".themes/dynamic/gtk-4.0/libadwaita.css".source = ./.themes/gtk-4.0/libadwaita.css;
   home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source = ./.themes/gtk-4.0/libadwaita-tweaks.css;
