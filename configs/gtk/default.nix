d631ffd9b5262592211d303c0c7ff14bc930814b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-04-26 19:20:55 +0700

    2026-04-26 19:20 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -1,13 +1,21 @@
-{ pkgs, config, ... }:
+{ pkgs, config, mkSymlink, ... }:
 
 let
   HOME = config.home.homeDirectory;
+  configs = {
+    "gtk.3-0/settings.ini" = "settings.ini";
+    "gtk.4-0/settings.ini" = "settings.ini";
+  };
 in {
   home.packages = with pkgs; [
     vimix-icon-theme
     cursor-memes
     vimix-gtk-themes
   ];
+
+  xdg.configFile = mkSymlink {
+    target = "gtk";
+  } configs;
 
   # Copy/symlink theme ke ~/.themes
   home.file.".themes/dynamic/gtk-4.0/assets".source = ./.themes/gtk-4.0/assets;
