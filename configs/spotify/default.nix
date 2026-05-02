88985d8371552392791d52405bc6301c1a73f0e0
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-02 21:56:46 +0700

    2026-05-02 21:56 | Update via SAVEFLAKE: configs/spotify/default.nix

--- configs/spotify/default.nix
+++ configs/spotify/default.nix
@@ -1,4 +1,4 @@
-{ inputs, pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 let
   spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
@@ -6,26 +6,73 @@ in
 {
   imports = [ inputs.spicetify-nix.homeManagerModules.default ];
 
-  home.packages = [
-    pkgs.spicetify-cli
-  ];
-
   programs.spicetify = {
-	  enable = true;
+    enable = true;
+    spicedSpotify = pkgs.spicetity; # The final spotify package after spicing.
+    spicetifyPackage = pkgs.spicetify-cli; # the spicetify-cli packages to use
+    spotifyPackage = pkgs.spotify;
+    spotifywmPackage = pkgs.spotifywm;
 
-	  enabledExtensions = with spicePkgs.extensions; [
-	    adblock
-	  ];
-	  enabledCustomApps = with spicePkgs.apps; [
-	    newReleases
-	  ];
-	  enabledSnippets = with spicePkgs.snippets; [
-	    rotatingCoverart
-	    pointer
-	  ];
+    # -- THEME & COLORSCHEME --
+    theme = {
+      patches = ''
+        {
+          "xpui.js_find_8008" = ",(\\w+=)32";
+          "xpui.js_repl_8008" = ",$\{1}56";
+        };'';
+    }
+    // spicePkgs.themes.dribbblish;
+    colorScheme = "dark";
 
-	  theme = spicePkgs.themes.dribbblish;
-	  colorScheme = "lunar";
+    # -- EKSTENSI & APLIKASI CUSTOM --
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      popupLyrics
+      autoSkipExplicit
+      playlistcons
+      formatColors
+      simpleBeautifulLyrics
+      addToQueueTop
+      coverAmbience
+      allOfArtist
+      catJamSynced
+    ];
+
+    enabledSnippets = with spicePkgs.snippets; [
+      rotatingCoverart
+      pointer
+    ];
+
+    enabledCustomApps = with spicePkgs.apps; [
+      localFiles
+      lyricsPlus
+      marketplace
+      ncsVisualizer
+      betterLibrary
+    ];
+
+    # Opsi alternatif jika menggunakan tema custom dari GitHub:
+    # theme = {
+    #   name = "Dribbblish";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "spicetify";
+    #     repo = "spicetify-themes";
+    #     rev = "02badb180c902f986a4ea4e4033e69fe8eec6a55";
+    #     hash = "sha256-KD9VfHtlN0BIHC4inlooxw5XC4xlHNC5evASRqP7pUA=";
+    #   };
+    # };
+    # customColorScheme = {
+    #   text = "ffffff";
+    #   main = "1e1e2e";
+    # };
+
+    # -- FITUR TAMBAHAN --
+    # -- TAMPILAN & WINDOW MANAGER --
+    # wayland = true; # Mengaktifkan flag native Wayland
+    windowManagerPatch = true; # Sangat berguna agar Spotify ter-render dengan baik di Tiling WM
+    alwaysEnableDevTools = true;
+    experimentalFeatures = true;
+    # spotifyLaunchFlags = "--enable-features=UseOzonePlatform --ozone-platform=wayland";
   };
 
   services.spotifyd = {
