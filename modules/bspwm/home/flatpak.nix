c117ca58ccf83188d2c4faa8406016d17349fe58
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-03 21:01:41 +0700

    2026-05-03 21:01 | Update via SAVEFLAKE

---
+++ modules/bspwm/home/flatpak.nix
@@ -0,0 +1,24 @@
+{ inputs, ... }:
+{
+  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
+
+  services.flatpak = {
+    enable = true;
+    
+    # Jika hanya ingin menambah beta tanpa menghapus flathub bawaan:
+    remotes = [
+      {
+        name = "flathub-beta";
+        location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+      }
+    ];
+
+    update.auto.enable = true;
+    uninstallUnmanaged = true;
+
+    packages = [
+      "flathub:app/com.spotify.Client//stable"
+    ];
+  };
+
+}
