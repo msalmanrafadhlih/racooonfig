c0327c09b01f3055f1c4435de8b9c2e23339cd34
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-03 07:28:07 +0700

    2026-05-03 07:28 | Update via SAVEFLAKE: configs/spotify/default.nix

--- configs/spotify/default.nix
+++ configs/spotify/default.nix
@@ -34,7 +34,25 @@ in
         };'';
     }
     // spicePkgs.themes.dribbblish;
-    colorScheme = "dark";
+    colorScheme = "custom";
+    customColorScheme = {
+      text               = "\$\{xrdb:text\}";
+      subtext            = "\$\{xrdb:subtext\}";
+      sidebar-text       = "\$\{xrdb:sidebar-text\}";
+      main               = "\$\{xrdb:main\}";
+      sidebar            = "\$\{xrdb:sidebar\}";
+      player             = "\$\{xrdb:player\}";
+      card               = "\$\{xrdb:card\}";
+      shadow             = "\$\{xrdb:shadow\}";
+      selected-row       = "\$\{xrdb:selected-row\}";
+      button             = "\$\{xrdb:button\}";
+      button-active      = "\$\{xrdb:button-active\}";
+      button-disabled    = "\$\{xrdb:button-disabled\}";
+      tab-active         = "\$\{xrdb:tab-active\}";
+      notification       = "\$\{xrdb:notification\}";
+      notification-error = "\$\{xrdb:notification-error\}";
+      misc               = "\$\{xrdb:misc\}";
+    };
 
     # -- EKSTENSI & APLIKASI CUSTOM --
     enabledExtensions = [
@@ -44,8 +62,7 @@ in
       popupLyrics
       playlistIcons
       formatColors
-      BeautifulLyrics
-      coverAmbience
+      beautifulLyrics
       allOfArtist
     ]);
 
