2d7ab54132145284312da1bf2f8e47e82e709429
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-05 04:05:48 +0700

    2026-05-05 04:05 | Update via SAVEFLAKE: modules/bspwm/systems/default.nix

--- modules/bspwm/systems/default.nix
+++ modules/bspwm/systems/default.nix
@@ -65,16 +65,4 @@
       };
     };
   };
-
-  environment = {
-    sessionVariables = {
-      XDG_CURRENT_DESKTOP = "bspwm";
-      XDG_SESSION_TYPE = "x11";
-    };
-    extraInit = ''
-      if [ -f "$HOME/.dynamic" ]; then
-        . "$HOME/.dynamic"
-      fi
-    '';
-  };
 }
