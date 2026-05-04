98da090bcaa83ee11820727e9e045ff0eb5b4194
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-04 18:57:11 +0700

    2026-05-04 18:57 | Update via SAVEFLAKE: flake.nix

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
