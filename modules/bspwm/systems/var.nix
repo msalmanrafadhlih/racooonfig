9aead53c171a75936f593ed1e759954474f79359
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-05 04:05:48 +0700

    2026-05-05 04:05 | Update via SAVEFLAKE: modules/bspwm/systems/var.nix

--- modules/bspwm/systems/var.nix
+++ modules/bspwm/systems/var.nix
@@ -1,6 +1,18 @@
-{ ... }:
+{ lib, ... }:
 
 {
+  environment = {
+    sessionVariables = {
+      XDG_CURRENT_DESKTOP = "bspwm";
+      XDG_SESSION_TYPE = "x11";
+    };
+    extraInit = ''
+      if [ -f "$HOME/.dynamic" ]; then
+        . "$HOME/.dynamic"
+      fi
+    '';
+  };
+
   systemd.tmpfiles.rules = [
     # Menggunakan grup wheel agar hanya admin yang bisa memodifikasi
     "d /usr/share/icons 0775 root wheel -"
@@ -8,8 +20,7 @@
   ];
 
   environment.variables = {
-    XCURSOR_PATH = "/usr/share/icons";
-
+    XCURSOR_PATH = lib.mkForce "/usr/share/icons:$HOME/.icons:$HOME/.local/share/icons";
     XDG_DATA_DIRS = [
       "/usr/share"
     ];
