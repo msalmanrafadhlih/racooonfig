638167250da82d04711e9068e519833167e9de86
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-05 07:52:53 +0700

    2026-05-05 07:52 | Update via SAVEFLAKE: modules/scripts/default.nix

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -5,19 +5,19 @@
 	  ./github-repos.nix
 	  ./media.nix
 	  ./run.nix
-	  ./volume.nix
 	  ./xyz.nix
 	  ./tar.nix
-	  ./brightness.nix
 	  ./colorscript.nix
 
+	  ./cli/pandoc.nix
+	  ./cli/volume.nix
 	  ./cli/01chat.nix
 	  ./cli/battery.nix
 	  ./cli/bookmarks.nix
-	  ./cli/pandoc.nix
+	  ./cli/brightness.nix
 	  ./cli/img-compress.nix
+	  ./cli/image-editor.nix
 	  ./cli/set-gtk-theme.nix
 	  ./cli/get-github-hash.nix
-	  ./cli/image-editor.nix
   ];
 }
