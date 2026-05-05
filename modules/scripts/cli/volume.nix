4f11a2e67c92ebf60af6ed613e6bbee372a02e49
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-05 14:20:01 +0700

    2026-05-05 14:20 | Update via SAVEFLAKE: modules/scripts/cli/volume.nix

--- modules/scripts/cli/volume.nix
+++ modules/scripts/cli/volume.nix
@@ -13,11 +13,13 @@ let
 
       vol=$(pamixer --get-volume)
       mute=$(pamixer --get-mute)
+      iconmute="$HOME/.config/Assets/Icons/mute.png"
+      iconvol="$HOME/.config/Assets/Icons/vol.png"
 
       if [ "$mute" = "true" ]; then
-        dunstify -t 2000 -i audio-volume-muted-symbolic -r 1002 -u low "Volume" "Muted"
+        dunstify -t 2000 -i "$iconmute" -r 1002 -u low "Volume" "Muted"
       else
-        dunstify -t 2000 -i audio-volume-high-symbolic \
+        dunstify -t 2000 -i "$iconvol" \
           -h int:value:"$vol" \
           -r 1002 \
           -u low "Volume" "''${vol}%"
