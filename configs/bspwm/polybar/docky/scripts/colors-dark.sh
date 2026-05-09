6890eedb01b9ffba9699a1f18639ccd2a80320a2
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-09 21:25:28 +0700

    2026-05-09 21:25 | Update via SAVEFLAKE: configs/bspwm/polybar/docky/scripts/colors-dark.sh

--- configs/bspwm/polybar/docky/scripts/colors-dark.sh
+++ configs/bspwm/polybar/docky/scripts/colors-dark.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/docky/colors.ini"
+PFILE="/tmp/polybar_${UID}.ini"
 RFILE="$HOME/.config/polybar/docky/scripts/rofi/colors.rasi"
 
 # Change colors
