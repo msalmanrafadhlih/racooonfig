c17869514f2aa582d64803926ed37d6ec67cefdc
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-09 21:25:29 +0700

    2026-05-09 21:25 | Update via SAVEFLAKE: configs/bspwm/polybar/grayblocks/scripts/pywal.sh

--- configs/bspwm/polybar/grayblocks/scripts/pywal.sh
+++ configs/bspwm/polybar/grayblocks/scripts/pywal.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/grayblocks/colors.ini"
+PFILE="/tmp/polybar_${UID}.ini"
 RFILE="$HOME/.config/polybar/grayblocks/scripts/rofi/colors.rasi"
 WFILE="$HOME/.cache/wal/colors.sh"
 
