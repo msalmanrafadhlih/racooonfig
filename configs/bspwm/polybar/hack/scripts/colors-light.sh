11e3513709f3fb5ebf8de34d6a8a6d0b7cdeedfe
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-09 21:25:29 +0700

    2026-05-09 21:25 | Update via SAVEFLAKE: configs/bspwm/polybar/hack/scripts/colors-light.sh

--- configs/bspwm/polybar/hack/scripts/colors-light.sh
+++ configs/bspwm/polybar/hack/scripts/colors-light.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/hack/colors.ini"
+PFILE="/tmp/polybar_${UID}.ini"
 RFILE="$HOME/.config/polybar/hack/scripts/rofi/colors.rasi"
 
 # Change colors
