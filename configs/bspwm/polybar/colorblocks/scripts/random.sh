a9ab5cc29f8337ee4e7005e5327ca0ec84284220
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-09 21:25:28 +0700

    2026-05-09 21:25 | Update via SAVEFLAKE: configs/bspwm/polybar/colorblocks/scripts/random.sh

--- configs/bspwm/polybar/colorblocks/scripts/random.sh
+++ configs/bspwm/polybar/colorblocks/scripts/random.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Color files
-PFILE="$HOME/.config/polybar/colorblocks/colors.ini"
+PFILE="/tmp/polybar_${UID}.ini"
 RFILE="$HOME/.config/polybar/colorblocks/scripts/rofi/colors.rasi"
 
 # Change colors
