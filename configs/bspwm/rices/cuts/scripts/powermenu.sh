6647fb8ff9e2383a467311c248833a5c46b4fb4d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-14 18:22:38 +0700

    2026-05-14 18:22 | Update via SAVEFLAKE: configs/bspwm/rices/cuts/scripts/powermenu.sh

--- configs/bspwm/rices/cuts/scripts/powermenu.sh
+++ configs/bspwm/rices/cuts/scripts/powermenu.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
 
-dir="$HOME/.config/$XDG_CURRENT_DESKTOP/rices/cuts/scripts/rofi"
+DIR="$HOME/.config/$XDG_CURRENT_DESKTOP"
+read -r RICE < "$DIR/.rice"
+dir="$DIR/rices/$RICE/scripts/rofi"
+
 uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
