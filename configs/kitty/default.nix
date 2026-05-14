ac5e7b9d213002ef70d556ee8e5831c80df7e074
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-14 20:03:22 +0700

    2026-05-14 20:03 | Update via SAVEFLAKE: configs/kitty/default.nix

--- configs/kitty/default.nix
+++ configs/kitty/default.nix
@@ -37,39 +37,47 @@ in
 
     extraConfig = ''
       include themes/racooonfig.conf
-      allow_remote_control yes
     '';
 
     settings = {
-      background_opacity = lib.mkForce "0.5";
-      placement_strategy = "center";
-      inactive_text_alpha = "0.7";
-      confirm_os_window_close = 0;
-      tab_bar_style = "powerline";
+      # fonts
+      bold_font = "JetBrainsMono NF";
+      italic_font = "VictorMono Nerd Font";
+      bold_italic_font = "VictorMono Nerd Font";
       font_size = "9.0";
 
+      background_opacity = lib.mkForce "0.5";
+      placement_strategy = "center";
+      confirm_os_window_close = 0;
+      tab_bar_style = "powerline";
+
       window_padding_width = "10";
-      window_alert_on_bell = false;
+      window_alert_on_bell = true;
       window_border_width = "3pt";
+
+      inactive_text_alpha = "0.7";
 
       hide_window_decorations = true;
       draw_minimal_borders = true;
-      enable_audio_bell = false;
+      enable_audio_bell = true;
       bell_on_tab = "🔔 ";
       remember_window_size = false;
+      allow_remote_control = "yes";
+      listen_on = "unix:/tmp/kitty";
 
       mouse_hide_wait = 1;
       select_by_word_characters = "@-./_~?&=%+#a";
       copy_on_select = true;
 
       cursor_trail = 1;
+      cursor_trail_decay = [ 0.1 0.4 ];
       cursor_shape = "block";
-      cursor_stop_blinking_after = 0;
+      cursor_shape_unfocused = "beam";
+      cursor_beam_thickness = 1.5;
+      cursor_stop_blinking_after = 15.0;
+      cursor_blink_interval = 1;
       scrollback_lines = 2000;
 
-      bold_font = "JetBrainsMono NF";
-      italic_font = "VictorMono Nerd Font";
-      bold_italic_font = "VictorMono Nerd Font";
     };
   };
 }
