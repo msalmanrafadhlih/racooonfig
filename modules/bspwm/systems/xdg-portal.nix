32b34a2636a1ed5a23a012944c2c478b0e711985
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-15 18:05:55 +0700

    2026-05-15 18:05 | Update via SAVEFLAKE: modules/bspwm/systems/xdg-portal.nix

--- modules/bspwm/systems/xdg-portal.nix
+++ modules/bspwm/systems/xdg-portal.nix
@@ -1,27 +1,36 @@
-{ pkgs, ... }:{
-	  #####################################
-	  ## XDG PORTAL (X11 Only)
-	  #####################################
-	  # Apps to System Communication
-	  # OpenFolder, ScreenSharing, OpenURL, Notif, Printing..
-		xdg.portal = {
-			enable = true;
-			xdgOpenUsePortal = true;
-			extraPortals = with pkgs; [
-			  xdg-desktop-portal-gtk
-			  # xdg-desktop-portal-cosmic
-			  # xdg-desktop-portal-gnome
-			  # xdg-desktop-portal-luminous
-			  # xdg-desktop-portal-phosh
-			  # xdg-desktop-portal-termfilechooser
-			  # xdg-desktop-portal-xapp
-	      # xdg-desktop-portal-wlr
-	      # xdg-desktop-portal-hyprland
-	    ];
-			config = {
-			  common = {
-			    default = ["gtk"];
-			  };
-			};
-		};
+{ pkgs, ... }:
+{
+  #####################################
+  ## XDG PORTAL (X11 Only)
+  #####################################
+  # Apps to System Communication
+  # OpenFolder, ScreenSharing, OpenURL, Notif, Printing..
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-gtk
+      # xdg-desktop-portal-cosmic
+      # xdg-desktop-portal-gnome
+      # xdg-desktop-portal-luminous
+      # xdg-desktop-portal-phosh
+      # xdg-desktop-portal-termfilechooser
+      # xdg-desktop-portal-xapp
+      # xdg-desktop-portal-wlr
+      # xdg-desktop-portal-hyprland
+    ];
+    config = {
+      common = {
+        default = [ "gtk" ];
+        # Eksplisit assign per-interface agar tidak ambigu
+        "org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
+        "org.freedesktop.impl.portal.AppChooser"  = [ "gtk" ];
+        "org.freedesktop.impl.portal.Screenshot"  = [ "gtk" ];
+        "org.freedesktop.impl.portal.OpenURI"     = [ "gtk" ];
+      };
+    };
+    bspwm = {
+      default = [ "gtk" ];
+    };
+  };
 }
