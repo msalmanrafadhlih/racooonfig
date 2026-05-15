feadd110bc26a7087593371f005fdecd2a6421e7
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-15 18:05:54 +0700

    2026-05-15 18:05 | Update via SAVEFLAKE: modules/bspwm/systems/var.nix

--- modules/bspwm/systems/var.nix
+++ modules/bspwm/systems/var.nix
@@ -10,6 +10,9 @@
     extraInit = ''
       export XCURSOR_PATH="/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
       export XDG_DATA_DIRS="/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+
+      if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+        eval $(dbus-launch --sh-syntax --exit-with-session)
     '';
   };
 
