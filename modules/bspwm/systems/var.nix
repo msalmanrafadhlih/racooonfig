f3ac32c1a4087577c3b95e19e916a3bb40f8e79a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-16 23:17:30 +0700

    2026-05-16 23:17 | Update via SAVEFLAKE: modules/bspwm/systems/var.nix

--- modules/bspwm/systems/var.nix
+++ modules/bspwm/systems/var.nix
@@ -2,17 +2,14 @@
 
 {
   environment = {
-    sessionVariables = {
-      XDG_CURRENT_DESKTOP = "bspwm";
-      XDG_SESSION_TYPE = "x11";
-    };
+    # sessionVariables = {
+    #   XDG_CURRENT_DESKTOP = "bspwm";
+    #   XDG_SESSION_TYPE = "x11";
+    # };
 
     extraInit = ''
       export XCURSOR_PATH="/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
       export XDG_DATA_DIRS="/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
-
-      if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-        eval $(dbus-launch --sh-syntax --exit-with-session)
     '';
   };
 
