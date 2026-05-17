24907fdca5d81e5d4adc81b71438c44acb732e31
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-17 19:06:45 +0700

    2026-05-17 19:06 | Update via SAVEFLAKE

---
+++ configs/bspwm/rices/blackwhite/scripts/pywal.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+read -r RICE < "$HOME/.config/$XDG_CURRENT_DESKTOP/.rice"
+SDIR="$HOME/.config/$XDG_CURRENT_DESKTOP/rices/$RICE/scripts"
+
+# Color files
+PFILE="/tmp/polybar_${UID}.ini"
+RFILE="$SDIR/rofi/colors.rasi"
+WFILE="$HOME/.cache/wal/colors.sh"
+
+# Get colors
+pywal_get() {
+feh --bg-fill "$1" && \
+	wal -i "$1" \
+	-n \
+  -q -e \
+  --vte \
+  -a 70 \
+  --contrast 4.5 \
+  --backend haishoku &
+
+}
+
+# Change colors
+change_color() {
+	# polybar
+	sed -i -e "s/background = #.*/background = $BG/g" $PFILE
+	sed -i -e "s/foreground = #.*/foreground = $FG/g" $PFILE
+	sed -i -e "s/primary = #.*/primary = $AC/g" $PFILE
+	
+	# rofi
+	cat > $RFILE <<- EOF
+	/* colors */
+
+	* {
+	  al:    #00000000;
+	  bg:    ${BG}FF;
+	  ac:    ${AC}FF;
+	  se:    ${AC}26;
+	  fg:    ${FG}FF;
+	}
+	EOF
+	
+	polybar-msg cmd restart
+}
+
+# Main
+if [[ -x "$(which wal)" ]]; then
+	if [[ "$1" ]]; then
+		pywal_get "$1"
+		sleep 0.5
+
+		# Source the pywal color file
+		if [[ -e "$WFILE" ]]; then
+			. "$WFILE"
+		else
+			echo 'Color file does not exist, exiting...'
+			exit 1
+		fi
+
+		BG=`printf "%s\n" "$background"`
+		FG=`printf "%s\n" "$foreground"`
+		AC=`printf "%s\n" "$color1"`
+
+		change_color
+	else
+		echo -e "[!] Please enter the path to wallpaper. \n"
+		echo "Usage : ./pywal.sh path/to/image"
+	fi
+else
+	echo "[!] 'pywal' is not installed."
+fi
