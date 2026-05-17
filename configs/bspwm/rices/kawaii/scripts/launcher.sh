85966c7dec445a9efb5ff723fc58777f55035325
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-17 19:06:42 +0700

    2026-05-17 19:06 | Update via SAVEFLAKE: configs/bspwm/rices/kawaii/scripts/launcher.sh

--- configs/bspwm/rices/kawaii/scripts/launcher.sh
+++ configs/bspwm/rices/kawaii/scripts/launcher.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-read -r RICE < "$HOME/.config/$XDG_CURRENT_DESKTOP/.rice"
-SDIR="$HOME/.config/$XDG_CURRENT_DESKTOP/rices/$RICE/scripts/rofi/launcher.rasi"
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme "$SDIR"
+read -r RICE < "$HOME/.config/$XDG_CURRENT_DESKTOP/.rice"
+SDIR="$HOME/.config/$XDG_CURRENT_DESKTOP/rices/$RICE/scripts"
+
+rofi -no-config -no-lazy-grab -show drun -modi drun -theme "$SDIR/rofi/launcher.rasi"
