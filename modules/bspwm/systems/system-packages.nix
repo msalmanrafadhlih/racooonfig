fcd51a59ec31dd54cb1f676ddcac92a9d61c7cdd
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-18 21:35:07 +0700

    2026-05-18 21:35 | Update via SAVEFLAKE: modules/bspwm/systems/system-packages.nix

--- modules/bspwm/systems/system-packages.nix
+++ modules/bspwm/systems/system-packages.nix
@@ -5,67 +5,77 @@ let
   inp = inputs.racooonfig.inputs;
 in
 {
-  environment.systemPackages =
-    [
-      inp.matugen.packages.${system}.default
-      inp.plank-reloaded.packages.${system}.default
-    ]
-    ++ (with pkgs; [
-      # ======== BSPWM Stuff
-      polybarFull
-      sxhkd
-      picom
-      rofi
-      eww
-      i3lock-color
+  environment.systemPackages = [
+    inp.matugen.packages.${system}.default
+    inp.plank-reloaded.packages.${system}.default
+  ]
+  ++ (with pkgs; [
+    # ======== BSPWM Stuff
+    polybarFull
+    sxhkd
+    picom
+    (symlinkJoin {
+      name = "rofi-webp-support";
+      paths = [ rofi ];
+      buildInputs = [ makeWrapper ];
+      postBuild = ''
+        wrapProgram $out/bin/rofi \
+          --prefix GDK_PIXBUF_MODULE_FILE : "${librsvg.out}/lib/gdk-pixbuf-2.0/2.10.0/loaders.cache" \
+          --set GDK_PIXBUF_MODULEDIR "${webp-pixbuf-loader}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
+      '';
+    })
+    eww
+    i3lock-color
 
-      # ======== TOOLS
-      sound-theme-freedesktop
-      libcanberra-gtk3
-      mpv-unwrapped
-      brightnessctl
-      flameshot
-      imagemagick
-      clipmenu
-      pamixer
-      dunst
-      maim
-      feh
-      bc
+    # ======== TOOLS
+    sound-theme-freedesktop
+    libcanberra-gtk3
+    mpv-unwrapped
+    brightnessctl
+    flameshot
+    imagemagick
+    clipmenu
+    pamixer
+    dunst
+    maim
+    feh
+    bc
 
-      # ======== X UTILS
-      (python3.withPackages (ps: with ps; [
+    # ======== X UTILS
+    (python3.withPackages (
+      ps: with ps; [
         pywal
         haishoku
-      ]))
+      ]
+    ))
 
-      shared-mime-info
-      xinit
-      xclip
-      psmisc
-      xsetroot
-      xrandr
-      xinput
-      xdotool
-      xcolor
-      xdo
-      xev
-      xxHash # ../../../configs/bspwm/bin/WallSync
-      calc 
+    shared-mime-info
+    xinit
+    xclip
+    psmisc
+    xsetroot
+    xrandr
+    xinput
+    xdotool
+    xcolor
+    xdo
+    xev
+    xxHash # ../../../configs/bspwm/bin/WallSync
+    calc
 
-      # ======== GTK
-      gtk2
-      gtk3
-      gtk4
-      dconf
-      dconf-editor
+    # ======== GTK
+    gtk2
+    gtk3
+    gtk4
+    dconf
+    dconf-editor
 
-      # ======== GTK engine
-      nwg-look
-      glib
+    # ======== GTK engine
+    nwg-look
+    glib
 
-      # tray
-      wmctrl # ../../../configs/bspwm/bin/WindowSwitcher
-      kdocker
-    ]);
+    # tray
+    wmctrl # ../../../configs/bspwm/bin/WindowSwitcher
+    kdocker
+  ]);
 }
