4b8b4c51a7b86927b3216869c6beb1c582f82b43
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-20 20:24:18 +0700

    2026-05-20 20:24 | Update via SAVEFLAKE: configs/suckless/default.nix

--- configs/suckless/default.nix
+++ configs/suckless/default.nix
@@ -1,22 +1,23 @@
 { inputs, pkgs, ... }:
-
+let
+  inp = inputs.racooonfig.inputs;
+in
 {
-	home.packages = [
-		inputs.st-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
-	];
+  home.packages = [
+    inp.st-nix.packages.${pkgs.stdenv.hostPlatform.system}.default
+  ];
 
-
- #  home.packages = with pkgs; [
-	# (pkgs.st.overrideAttrs (_: {
-	# 	src = inputs.st-src;
-	# 	patches = [ ];
-	# }))
- #    (pkgs.dmenu.overrideAttrs (_: {
- #        src = inputs.dmenu-src;
- #        patches = [ ];
- #    }))
-	# slock
-	# surf
- #  ];
+  #  home.packages = with pkgs; [
+  # (pkgs.st.overrideAttrs (_: {
+  # 	src = inputs.st-src;
+  # 	patches = [ ];
+  # }))
+  #    (pkgs.dmenu.overrideAttrs (_: {
+  #        src = inputs.dmenu-src;
+  #        patches = [ ];
+  #    }))
+  # slock
+  # surf
+  #  ];
 
 }
