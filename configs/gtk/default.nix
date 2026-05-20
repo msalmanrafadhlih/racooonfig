0a9f48bf7e20c0b089462802ad1ac89ed58d9abe
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-20 20:52:42 +0700

    2026-05-20 20:52 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -1,8 +1,12 @@
-{ pkgs, config, ... }:
-
+{
+  pkgs,
+  config,
+  ...
+}:
 let
   HOME = config.home.homeDirectory;
-in {
+in
+{
   home.packages = with pkgs; [
     vimix-icon-theme
     cursor-memes
@@ -12,7 +16,8 @@ in {
   # Copy/symlink theme ke ~/.themes
   home.file.".themes/dynamic/gtk-4.0/assets".source = ./.themes/gtk-4.0/assets;
   home.file.".themes/dynamic/gtk-4.0/libadwaita.css".source = ./.themes/gtk-4.0/libadwaita.css;
-  home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source = ./.themes/gtk-4.0/libadwaita-tweaks.css;
+  home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source =
+    ./.themes/gtk-4.0/libadwaita-tweaks.css;
 
   home.file.".themes/dynamic/gtk-3.0".source = ./.themes/gtk-3.0;
   home.file.".themes/dynamic/index.theme".source = ./.themes/index.theme;
