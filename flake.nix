bd0e806677496082e62daf4f3d02bc2f87392c1b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-20 18:54:24 +0700

    2026-05-20 18:54 | Update via SAVEFLAKE: flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,32 @@
 {
   description = "A declarative Users configuration built with intention.";
 
+  outputs =
+    { self, ... }@inputs:
+    let
+      # mapping nix files & directories
+      myLibs = import ./.lib { inherit inputs self; };
+    in
+    {
+      homeModules.racooonfig = {
+        _module.args = { inherit (myLibs.mapping) mapFile mapDir mapAll; };
+        imports = [ ./modules/homeModules.nix ];
+      };
+
+      nixosModules.racooonfig = {
+        _module.args = { inherit (myLibs.mapping) mapFile mapDir mapAll; };
+        imports = [ ./modules/nixosModules.nix ];
+      };
+
+      legacyPackages = myLibs.legacyPackages; # applies overlays.default to nixpkgs.legacyPackages
+      devShells      = myLibs.devShells;
+      packages       = myLibs.packages; # custom packages built against nixpkgs
+      overlays       = myLibs.overlays; # overlays.default is the sum of all the overlays
+    };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
 
     ## ---- System Packages
     xytz = {
@@ -19,11 +38,6 @@
       url = "github:TQ-See/st-flexipatch";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # stylix = {
-    #   url = "github:nix-community/stylix";
-    #   inputs.nixpkgs.follows = "nixpkgs";
-    # };
 
     plank-reloaded = {
       url = "github:zquestz/plank-reloaded";
@@ -64,48 +78,6 @@
     yazi-compress = {
       url = "github:KKV9/compress.yazi";
       flake = false;
-    };
-  };
-
-  outputs = { self, home-manager, ... }@inputs:
-  let
-    mkHome = hostname: { user ? "tquilla", system ? "x86_64-linux", module }: 
-      home-manager.lib.homeManagerConfiguration {
-        extraSpecialArgs = { inherit hostname system; username = user;  };
-        pkgs    = inputs.nixpkgs.legacyPackages.${system};
-        modules = module; 
-      };
-
-    mkModule = extraModules: {
-      imports = [ ./modules  ] ++ extraModules;
-    };
-  in
-  {
-    homeModules = {
-      default  = mkModule [ ./modules/bspwm/home ];
-      niri     = mkModule [ ./modules/niri/home ];
-      hyprland = mkModule [ ./modules/hyprland/home ];
-    };
-
-    # ✅ nixos module yang benar
-    nixosModules = {
-      bspwm-core = {
-        imports = [
-          ./modules/bspwm/systems
-          ./modules/scripts/cli
-        ];
-      };
-    };
-
-    # keep standalone homeConfigurations
-    # for `home-manager switch --flake .#bspwm / #niri / #hyprland`
-    homeConfigurations = {
-      bspwm    = mkHome { module = [ self.homeModules.default ]; };
-      niri     = mkHome { module = [ self.homeModules.niri ]; };
-      hyprland = mkHome { module = [ self.homeModules.hyprland ]; };
-    # i rarely use this homeConfigurations module, i use (sistem-)specialisations instead
-    # so, this code is useless, u can delete it.
-    # for me, i just want to keep it.
     };
   };
 }
