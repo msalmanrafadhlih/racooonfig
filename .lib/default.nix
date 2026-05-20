dd837aaae117f165c1bd091fbff943d2a5830f88
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-20 19:06:44 +0700

    2026-05-20 19:06 | Update via SAVEFLAKE: .lib/default.nix

--- .lib/default.nix
+++ .lib/default.nix
@@ -20,7 +20,7 @@ let
 
   mapping        = import ./map-lib.nix         args;
   overlays       = import ./overlays.nix        args;
-  devShells      = import ./devShells.nix       args;
+  devShells      = import ./devShells           args;
   legacyPackages = import ./legacyPackages.nix  args;
 in
 {
