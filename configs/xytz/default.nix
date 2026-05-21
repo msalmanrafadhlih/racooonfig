45368b3f414f93cdaf8bc1e853082a8d27a2c1ae
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/xytz/default.nix

--- configs/xytz/default.nix
+++ configs/xytz/default.nix
@@ -1,10 +1,20 @@
-{ inputs, ... }:
+{
+  inputs,
+  config,
+  lib,
+  ...
+}:
 let
   inp = inputs.racooonfig.inputs;
+  cfg = config.racooonfig;
 in
 {
   imports = [ inp.xytz.homeManagerModules.default ];
-  programs.xytz = {
-    enable = true;
+
+  config = lib.mkIf (builtins.elem "xytz" cfg.listConfigurations) {
+
+    programs.xytz = {
+      enable = true;
+    };
   };
 }
