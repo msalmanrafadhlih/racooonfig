3800be03c89dc767da084101dff6e4ea3c5c7494
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/nano/default.nix

--- configs/nano/default.nix
+++ configs/nano/default.nix
@@ -1,17 +1,25 @@
-{ mkSymlink, ... }:
+{
+  mkSymlink,
+  lib,
+  config,
+  ...
+}:
 
 let
   configs = {
-		".config/nano/extra"   = "extra";
-		".config/nano/default" = "default";
+    ".config/nano/extra" = "extra";
+    ".config/nano/default" = "default";
 
-		".nanorc" = "nanorc";
+    ".nanorc" = "nanorc";
   };
-
+  cfg = config.racooonfig;
 in
 
 {
-  home.file = mkSymlink {
-    target = "nano";
-  } configs;
+  config = lib.mkIf (builtins.elem "nano" cfg.listConfigurations) {
+    home.file = mkSymlink {
+      target = "nano";
+    } configs;
+  };
+
 }
