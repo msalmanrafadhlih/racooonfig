9ac3764c44b091885d004c3e970218af4e4d8007
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/nwg-drawer/default.nix

--- configs/nwg-drawer/default.nix
+++ configs/nwg-drawer/default.nix
@@ -1,78 +1,89 @@
 # nwg-drawer, sebuah Application Launcher (menu aplikasi) yang bergaya "drawer"
 # (seperti laci aplikasi di smartphone Android).
-{ pkgs, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (builtins.elem "nwg-drawer" cfg.listConfigurations) {
+    home.packages = with pkgs; [
+      nwg-drawer
 
-  home.packages = with pkgs; [
-    nwg-drawer
+      (writeShellScriptBin "drawer" ''
+        killall nwg-drawer
+        nwg-drawer -mb 128 -ml 128 -mr 128 -mt 128 -nocats -nofs -open -term kitty
+      '')
+    ];
 
-    (writeShellScriptBin "drawer" ''
-      killall nwg-drawer
-      nwg-drawer -mb 128 -ml 128 -mr 128 -mt 128 -nocats -nofs -open -term kitty
-    '')
-  ];
+    home.file.".config/nwg-drawer/drawer.css".text = ''
+      * {
+          color: #cdd6f4;
+          border: none;
+          transition: all 300ms ease-in-out;
+      }
 
-  home.file.".config/nwg-drawer/drawer.css".text = ''
-    * {
-        color: #cdd6f4;
-        border: none;
-        transition: all 300ms ease-in-out;
-    }
+      window {
+          border: none;
+          background-color: rgba(17, 17, 27, 0.3);
+          /* border: solid 1px #313244; */
+          border-radius: 10px;
+      }
 
-    window {
-        border: none;
-        background-color: rgba(17, 17, 27, 0.3);
-        /* border: solid 1px #313244; */
-        border-radius: 10px;
-    }
+      /* search entry */
+      entry {
+          background-color: rgba(17, 17, 27, 0); /* transparent */
+          color: #cdd6f4;
+          border-radius: 10px;
+      }
 
-    /* search entry */
-    entry {
-        background-color: rgba(17, 17, 27, 0); /* transparent */
-        color: #cdd6f4;
-        border-radius: 10px;
-    }
+      button,
+      image {
+          all: unset;
+          border-radius: 10px;
+          padding: 4px;
+          color: #cdd6f4;
+          background: none;
+          border: none;
+          transition: all 200ms ease-in-out;
+      }
 
-    button,
-    image {
-        all: unset;
-        border-radius: 10px;
-        padding: 4px;
-        color: #cdd6f4;
-        background: none;
-        border: none;
-        transition: all 200ms ease-in-out;
-    }
+      scrollbar slider {
+          background: none;
+          border: none;
+          min-width: 0;
+          min-height: 0;
+      }
 
-    scrollbar slider {
-        background: none;
-        border: none;
-        min-width: 0;
-        min-height: 0;
-    }
+      button {
+          border: none;
+      }
 
-    button {
-        border: none;
-    }
+      button:hover {
+          background-color: #313244; /* surface0 */
+      }
 
-    button:hover {
-        background-color: #313244; /* surface0 */
-    }
+      #pinned-box {
+          padding-bottom: 5px;
+          border-bottom: 1px dotted #cdd6f4;
+      }
 
-    #pinned-box {
-        padding-bottom: 5px;
-        border-bottom: 1px dotted #cdd6f4;
-    }
+      #files-box {
+          padding: 5px;
+          border: 1px dotted #cdd6f4;
+          border-radius: 15px;
+      }
 
-    #files-box {
-        padding: 5px;
-        border: 1px dotted #cdd6f4;
-        border-radius: 15px;
-    }
+      /* math operation result label */
+      #math-label {
+          font-weight: bold;
+          font-size: 16px;
+      }
+    '';
+  };
 
-    /* math operation result label */
-    #math-label {
-        font-weight: bold;
-        font-size: 16px;
-    }
-  '';
 }
