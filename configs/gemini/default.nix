06244d6b9cf9e355d7c4d48cfd5351bd6f047aa8
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/gemini/default.nix

--- configs/gemini/default.nix
+++ configs/gemini/default.nix
@@ -1,14 +1,26 @@
-{ pkgs, mkSymlink, ... }: let
+{
+  pkgs,
+  mkSymlink,
+  lib,
+  config,
+  ...
+}:
+let
   configs = {
     ".gemini/settings.json" = "./settings.json";
   };
-in {
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (builtins.elem "gemini" cfg.listConfigurations) {
+    home.file = mkSymlink {
+      target = "gemini";
+    } configs;
 
-  home.file = mkSymlink {
-    target = "gemini";
-  } configs;
+    home.packages = with pkgs; [
+      gemini-cli
+    ];
 
-  home.packages = with pkgs; [
-    gemini-cli
-  ];
+  };
+
 }
