6ab9fea71bdbe87d442acfd36f3322b25c2105b6
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 22:46:57 +0700

    2026-05-21 22:46 | Update via SAVEFLAKE

---
+++ modules/homeOptions/packages.nix
@@ -0,0 +1,71 @@
+{ pkgs, inputs, ... }:
+
+let
+  system = pkgs.stdenv.hostPlatform.system;
+  inp = inputs.racooonfig.inputs;
+in
+{
+  home.packages = [
+    inp.matugen.packages.${system}.default
+    inp.plank-reloaded.packages.${system}.default
+  ]
+  ++ (with pkgs; [
+    # ======== BSPWM Stuff
+    polybarFull
+    sxhkd
+    picom
+    eww
+    i3lock-color
+
+    # ======== TOOLS
+    sound-theme-freedesktop
+    libcanberra-gtk3
+    mpv-unwrapped
+    brightnessctl
+    flameshot
+    imagemagick
+    clipmenu
+    pamixer
+    dunst
+    maim
+    feh
+    bc
+
+    # ======== X UTILS
+    (python3.withPackages (
+      ps: with ps; [
+        pywal
+        haishoku
+      ]
+    ))
+
+    shared-mime-info
+    xinit
+    xclip
+    psmisc
+    xsetroot
+    xrandr
+    xinput
+    xdotool
+    xcolor
+    xdo
+    xev
+    xxHash # ../../../configs/bspwm/bin/WallSync
+    calc
+
+    # ======== GTK
+    gtk2
+    gtk3
+    gtk4
+    dconf
+    dconf-editor
+
+    # ======== GTK engine
+    nwg-look
+    glib
+
+    # tray
+    wmctrl # ../../../configs/bspwm/bin/WindowSwitcher
+    kdocker
+  ]);
+}
