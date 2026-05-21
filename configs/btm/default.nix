e3cfd07fc96ed9d21700c1529d81cd11a736f8ce
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/btm/default.nix

--- configs/btm/default.nix
+++ configs/btm/default.nix
@@ -1,22 +1,40 @@
-{ pkgs, ... }: {
-  programs.bottom = { enable = true; };
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (builtins.elem "bottom" cfg.listConfigurations) {
 
-  catppuccin.bottom = {
-    enable = true;
-    flavor = "mocha";
+    programs.bottom = {
+      enable = true;
+    };
+
+    catppuccin.bottom = {
+      enable = true;
+      flavor = "mocha";
+    };
+
+    xdg.desktopEntries.bottom = {
+      name = "bottom";
+      genericName = "System Monitor";
+      comment = "A customizable cross-platform graphical process/system monitor for the terminal.";
+      exec = "kitty -e btm";
+      terminal = false;
+      type = "Application";
+      icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/bluefish.svg";
+      categories = [
+        "Utility"
+        "System"
+        "ConsoleOnly"
+        "Monitor"
+      ];
+      startupNotify = false;
+    };
   };
 
-  xdg.desktopEntries.bottom = {
-    name = "bottom";
-    genericName = "System Monitor";
-    comment =
-      "A customizable cross-platform graphical process/system monitor for the terminal.";
-    exec = "kitty -e btm";
-    terminal = false;
-    type = "Application";
-    icon =
-      "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/bluefish.svg";
-    categories = [ "Utility" "System" "ConsoleOnly" "Monitor" ];
-    startupNotify = false;
-  };
 }
