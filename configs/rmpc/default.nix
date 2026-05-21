f160c39e960281e643a3acd6ff6b522b1a264004
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:32 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: configs/rmpc/default.nix

--- configs/rmpc/default.nix
+++ configs/rmpc/default.nix
@@ -1,22 +1,33 @@
-{ pkgs, mkSymlink, ... }:
+{
+  pkgs,
+  mkSymlink,
+  lib,
+  config,
+  ...
+}:
 
 let
   configs = {
-    "rmpc/themes"     = "themes";
-    "rmpc/utils"      = "utils";
+    "rmpc/themes" = "themes";
+    "rmpc/utils" = "utils";
     "rmpc/config.ron" = "config.ron";
-    "rmpc/inspect"    = "inspect_log.sh";
-    "rmpc/playcount"  = "increment_play_count";
+    "rmpc/inspect" = "inspect_log.sh";
+    "rmpc/playcount" = "increment_play_count";
   };
+  cfg = config.racooonfig;
 in
 {
-  programs.rmpc = {
-    enable = true;
-    package = pkgs.rmpc;
+  config = lib.mkIf (builtins.elem "rmpc" cfg.listConfigurations) {
+
+    programs.rmpc = {
+      enable = true;
+      package = pkgs.rmpc;
+    };
+
+    # Langsung targetkan ke subfolder rmpc
+    xdg.configFile = mkSymlink {
+      target = "rmpc";
+    } configs;
   };
 
-  # Langsung targetkan ke subfolder rmpc
-  xdg.configFile = mkSymlink {
-    target = "rmpc";
-  } configs;
 }
