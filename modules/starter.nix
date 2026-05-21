e1b9804d04073de114b43203f6cbffc6bea55d88
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 06:33:33 +0700

    2026-05-22 06:33 | Update via SAVEFLAKE: modules/starter.nix

--- modules/starter.nix
+++ modules/starter.nix
@@ -1,1 +1,31 @@
-{ ... }: {}
+{
+  lib,
+  inputs,
+  config,
+  ...
+}:
+let
+  mapFile = inputs.racooonfig.mapFile;
+  mapDir  = inputs.racooonfig.mapFile;
+  cfg     = config.racooonfig;
+
+  wmMap = {
+    bspwm    = bspwm;
+    hyprland = hyprland;
+    niri     = niri;
+  };
+
+  bspwm    = mapFile ./nixosModules/bspwm [ ] { };
+  hyprland = mapFile ./nixosModules/hyprland [ ] { };
+  niri     = mapFile ./nixosModules/niri [ ] { };
+  home     = [ ./homeModules/default.nix ];
+in
+{
+  imports =
+    lib.optional cfg.enable ./nixosModules/default.nix
+    ++ lib.concatMap (wm: wmMap.${wm} or cfg.listConfigurations) home
+    ++ lib.optionals (cfg.enable && builtins.elem "bspwm" cfg.windowManager) bspwm
+    ++ lib.optionals (cfg.enable && builtins.elem "hyprland" cfg.windowManager) hyprland
+    ++ lib.optionals (cfg.enable && builtins.elem "niri" cfg.windowManager) niri
+    ++ mapDir ../configs [ "bspwm" "hyprland" "niri" ] { };
+}
