f3c1c8c5ed55feb34d50502285d426475deb2ab6
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-21 16:13:32 +0700

    2026-05-21 16:13 | Update via SAVEFLAKE: .lib/default.nix

--- .lib/default.nix
+++
@@ -1,7 +0,0 @@
-{ inputs, ... }:
-{
-  packages       = inputs.racooon.packages; # custom packages built against nixpkgs
-  mapAll         = inputs.racooon.mapAll;
-  mapDir         = inputs.racooon.mapAll;
-  mapFile        = inputs.racooon.mapAll;
-}
