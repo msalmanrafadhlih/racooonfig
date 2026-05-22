3d4332a76c2fd867795b51a0ec9dc381989bf241
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/bspwm/packages.nix

--- modules/homeModules/bspwm/packages.nix
+++ modules/homeModules/bspwm/packages.nix
@@ -1,71 +1,80 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}:
 
 let
   system = pkgs.stdenv.hostPlatform.system;
   inp = inputs.racooonfig.inputs;
+  cfg = config.racooonfig;
 in
 {
-  home.packages = [
-    inp.matugen.packages.${system}.default
-    inp.plank-reloaded.packages.${system}.default
-  ]
-  ++ (with pkgs; [
-    # ======== BSPWM Stuff
-    polybarFull
-    sxhkd
-    picom
-    eww
-    i3lock-color
+  config = lib.mkIf (cfg.homeManager && builtins.elem "bspwm" cfg.listConfigurations) {
+    home.packages = [
+      inp.matugen.packages.${system}.default
+      inp.plank-reloaded.packages.${system}.default
+    ]
+    ++ (with pkgs; [
+      # ======== BSPWM Stuff
+      polybarFull
+      sxhkd
+      picom
+      eww
+      i3lock-color
 
-    # ======== TOOLS
-    sound-theme-freedesktop
-    libcanberra-gtk3
-    mpv-unwrapped
-    brightnessctl
-    flameshot
-    imagemagick
-    clipmenu
-    pamixer
-    dunst
-    maim
-    feh
-    bc
+      # ======== TOOLS
+      sound-theme-freedesktop
+      libcanberra-gtk3
+      mpv-unwrapped
+      brightnessctl
+      flameshot
+      imagemagick
+      clipmenu
+      pamixer
+      dunst
+      maim
+      feh
+      bc
 
-    # ======== X UTILS
-    (python3.withPackages (
-      ps: with ps; [
-        pywal
-        haishoku
-      ]
-    ))
+      # ======== X UTILS
+      (python3.withPackages (
+        ps: with ps; [
+          pywal
+          haishoku
+        ]
+      ))
 
-    shared-mime-info
-    xinit
-    xclip
-    psmisc
-    xsetroot
-    xrandr
-    xinput
-    xdotool
-    xcolor
-    xdo
-    xev
-    xxHash # ../../../configs/bspwm/bin/WallSync
-    calc
+      shared-mime-info
+      xinit
+      xclip
+      psmisc
+      xsetroot
+      xrandr
+      xinput
+      xdotool
+      xcolor
+      xdo
+      xev
+      xxHash # ../../../configs/bspwm/bin/WallSync
+      calc
 
-    # ======== GTK
-    gtk2
-    gtk3
-    gtk4
-    dconf
-    dconf-editor
+      # ======== GTK
+      gtk2
+      gtk3
+      gtk4
+      dconf
+      dconf-editor
 
-    # ======== GTK engine
-    nwg-look
-    glib
+      # ======== GTK engine
+      nwg-look
+      glib
 
-    # tray
-    wmctrl # ../../../configs/bspwm/bin/WindowSwitcher
-    kdocker
-  ]);
+      # tray
+      wmctrl # ../../../configs/bspwm/bin/WindowSwitcher
+      kdocker
+    ]);
+  };
 }
