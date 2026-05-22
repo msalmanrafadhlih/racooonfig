2a96a77fcee3904a91f8788275c54add54567952
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:30 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/nixosModules/bspwm/thunar.nix

--- modules/nixosModules/bspwm/thunar.nix
+++ modules/nixosModules/bspwm/thunar.nix
@@ -1,17 +1,26 @@
-{ pkgs, lib, ... }: {
-
-
-  programs = {
-		thunar = {
-			enable = lib.mkDefault true;
-			plugins = with pkgs; [
-				thunar-volman
-				thunar-dropbox-plugin
-				thunar-vcs-plugin
-				thunar-media-tags-plugin
-				thunar-shares-plugin
-				thunar-archive-plugin
-			];
-		};
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (cfg.enable && builtins.elem "bspwm" cfg.windowManager) {
+    programs = {
+      thunar = {
+        enable = lib.mkDefault true;
+        plugins = with pkgs; [
+          thunar-volman
+          thunar-dropbox-plugin
+          thunar-vcs-plugin
+          thunar-media-tags-plugin
+          thunar-shares-plugin
+          thunar-archive-plugin
+        ];
+      };
+    };
   };
 }
