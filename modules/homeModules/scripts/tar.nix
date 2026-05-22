58de0a5c22f620988a6408b8fbd2062409f6e9e4
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/scripts/tar.nix

--- modules/homeModules/scripts/tar.nix
+++ modules/homeModules/scripts/tar.nix
@@ -1,33 +1,36 @@
+{ config, lib, ... }:
 {
-  home.file.".local/bin/targz" = {
-    text = ''
-		#!/usr/bin/env bash
-		# tar - simple tar.gz installer for cursor/icon themes
+  config = lib.mkIf config.racooonfig.homeManager {
+    home.file.".local/bin/targz" = {
+      text = ''
+        		#!/usr/bin/env bash
+        		# tar - simple tar.gz installer for cursor/icon themes
 
-		# Tanya path file tar.gz
-		read -p "Masukkan path ke file .tar.gz: " TARFILE
-		TARFILE="''${TARFILE/#\~/$HOME}"
+        		# Tanya path file tar.gz
+        		read -p "Masukkan path ke file .tar.gz: " TARFILE
+        		TARFILE="''${TARFILE/#\~/$HOME}"
 
-		# cek apakah file ada
-		if [ ! -f "$TARFILE" ]; then
-		  echo "❌ File tidak ditemukan: $TARFILE"
-		  exit 1
-		fi
+        		# cek apakah file ada
+        		if [ ! -f "$TARFILE" ]; then
+        		  echo "❌ File tidak ditemukan: $TARFILE"
+        		  exit 1
+        		fi
 
-		# Tanya tujuan (default /home/$USER/)
-		read -p "Masukkan path tujuan [default: /home/$USER/Documents]: " DEST
-		DEST="''${DEST/#\~/$HOME/Documents}"
-		DEST="''${DEST:-/home/$USER/Documents}"
-		  
-		# Buat folder tujuan kalau belum ada
-		mkdir -p "$DEST"
+        		# Tanya tujuan (default /home/$USER/)
+        		read -p "Masukkan path tujuan [default: /home/$USER/Documents]: " DEST
+        		DEST="''${DEST/#\~/$HOME/Documents}"
+        		DEST="''${DEST:-/home/$USER/Documents}"
+        		  
+        		# Buat folder tujuan kalau belum ada
+        		mkdir -p "$DEST"
 
-		# Ekstrak
-		echo "📦 Mengekstrak $TARFILE ke $DEST ..."
-		tar -xvzf "$TARFILE" -C "$DEST"
+        		# Ekstrak
+        		echo "📦 Mengekstrak $TARFILE ke $DEST ..."
+        		tar -xvzf "$TARFILE" -C "$DEST"
 
-		echo "✅ Selesai! File sudah diekstrak ke: $DEST"
-    '';
-    executable = true;
+        		echo "✅ Selesai! File sudah diekstrak ke: $DEST"
+      '';
+      executable = true;
+    };
   };
 }
