bdb012cd606bc855a1a1498655b2cfeb0103f32a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: configs/firefox/firefox.nix

--- configs/firefox/firefox.nix
+++ configs/firefox/firefox.nix
@@ -1,14 +1,27 @@
-{ pkgs, ... }: {
-  xdg.desktopEntries.firefox_focus = {
-    name = "Firefox Focus";
-    genericName = "Web Browser";
-    comment = "New Private Window";
-    exec = "firefox --private-window %U";
-    terminal = false;
-    type = "Application";
-    icon =
-      "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/firefox-developer.svg";
-    categories = [ "Network" "WebBrowser" ];
-    startupNotify = true;
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (builtins.elem "firefox" cfg.listConfigurations) {
+    xdg.desktopEntries.firefox_focus = {
+      name = "Firefox Focus";
+      genericName = "Web Browser";
+      comment = "New Private Window";
+      exec = "firefox --private-window %U";
+      terminal = false;
+      type = "Application";
+      icon = "${pkgs.tela-circle-icon-theme}/share/icons/Tela-circle/scalable/apps/firefox-developer.svg";
+      categories = [
+        "Network"
+        "WebBrowser"
+      ];
+      startupNotify = true;
+    };
   };
 }
