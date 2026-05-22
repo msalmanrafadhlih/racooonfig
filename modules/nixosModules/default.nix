2578a5cc53a718eb0de2a14267a1330c98fcd839
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:30 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/nixosModules/default.nix

--- modules/nixosModules/default.nix
+++ modules/nixosModules/default.nix
@@ -2,58 +2,67 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
 }:
 let
   cfg = config.racooonfig;
+  mapAll = inputs.racooonfig.mapAll;
 in
 {
-  environment = {
-    extraInit = ''
-      export XCURSOR_PATH="/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
-      export XDG_DATA_DIRS="/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
-    '';
-  };
+  imports  = mapAll ./bspwm    [ ] { }
+          ++ mapAll ./hyprland [ ] { }
+          ++ mapAll ./niri     [ ] { };
 
-  systemd.tmpfiles.rules = [
-    "d /usr/share/icons 0775 root wheel -"
-    "d /usr/share/themes 0775 root wheel -"
-  ];
+  config = lib.mkMerge [
+    (lib.mkIf cfg.enable {
+      environment = {
+        extraInit = ''
+          export XCURSOR_PATH="/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
+          export XDG_DATA_DIRS="/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+        '';
+      };
 
-  config = lib.mkIf cfg.enableDisplayManager {
-    environment.systemPackages = [
-      pkgs.qylock-sddm-theme
-      pkgs.cursor-memes
-    ];
-    services = {
-      displayManager = {
-        sddm = {
-          enable = lib.mkDefault true;
-          theme = lib.mkDefault "orbital";
-          setupScript = ''
-            ${pkgs.xrdb}/bin/xrdb -merge - <<EOF
-            Xcursor.theme: Skyrim-by-ru5tyshark-cursors
-            Xcursor.size: 32
-            EOF
-          '';
+      systemd.tmpfiles.rules = [
+        "d /usr/share/icons 0775 root wheel -"
+        "d /usr/share/themes 0775 root wheel -"
+      ];
+    })
+    (lib.mkIf (cfg.enable && cfg.enableDisplayManager) {
+      environment.systemPackages = [
+        pkgs.qylock-sddm-theme
+        pkgs.cursor-memes
+      ];
+      services = {
+        displayManager = {
+          sddm = {
+            enable = lib.mkDefault true;
+            theme = lib.mkDefault "orbital";
+            setupScript = ''
+              ${pkgs.xrdb}/bin/xrdb -merge - <<EOF
+              Xcursor.theme: Skyrim-by-ru5tyshark-cursors
+              Xcursor.size: 32
+              EOF
+            '';
 
-          # PENTING: SDDM butuh ini agar module QML terbaca oleh greeter
-          # Tambahkan paket tema dan dependensi QML yang wajib
-          extraPackages = [
-            pkgs.kdePackages.qtmultimedia
-            pkgs.kdePackages.qt5compat
-            pkgs.kdePackages.qtsvg
-            pkgs.kdePackages.qtdeclarative # qt6-declarative di dokumen
+            # PENTING: SDDM butuh ini agar module QML terbaca oleh greeter
+            # Tambahkan paket tema dan dependensi QML yang wajib
+            extraPackages = [
+              pkgs.kdePackages.qtmultimedia
+              pkgs.kdePackages.qt5compat
+              pkgs.kdePackages.qtsvg
+              pkgs.kdePackages.qtdeclarative # qt6-declarative di dokumen
 
-            # GStreamer Plugins (Wajib untuk video background di qylock)
-            pkgs.gst_all_1.gst-plugins-base
-            pkgs.gst_all_1.gst-plugins-good
-            pkgs.gst_all_1.gst-plugins-bad
-            pkgs.gst_all_1.gst-plugins-ugly
-            pkgs.gst_all_1.gst-libav
-          ];
+              # GStreamer Plugins (Wajib untuk video background di qylock)
+              pkgs.gst_all_1.gst-plugins-base
+              pkgs.gst_all_1.gst-plugins-good
+              pkgs.gst_all_1.gst-plugins-bad
+              pkgs.gst_all_1.gst-plugins-ugly
+              pkgs.gst_all_1.gst-libav
+            ];
+          };
         };
       };
-    };
-  };
+    })
+  ];
 }
