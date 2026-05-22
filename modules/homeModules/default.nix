5825a7f2c6cae69517cc667e0c9968a094cd55f4
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/default.nix

--- modules/homeModules/default.nix
+++ modules/homeModules/default.nix
@@ -33,34 +33,36 @@ let
 in
 
 {
-  _module.args = { inherit mkSymlink; };
 
-  imports =
-    lib.optionals (builtins.elem "bspwm" cfg.listConfigurations) [ ./bspwm ]
-    ++ lib.optionals (builtins.elem "hyprland" cfg.listConfigurations) [ ./hyprland ]
-    ++ lib.optionals (builtins.elem "niri" cfg.listConfigurations) [ ./niri ]
-    ++ mapAll ./scripts [ ] { };
+  imports =  mapAll ./bspwm    [ ] { }
+          ++ mapAll ./hyprland [ ] { }
+          ++ mapAll ./niri     [ ] { }
+          ++ mapAll ./scripts  [ ] { };
 
-  home.activation = {
-    setupDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      		  # Buat folder .dotfiles jika belum ada
-      		  mkdir -p ${dotfiles_path}
+  config = lib.mkIf cfg.homeManager {
+    _module.args = { inherit mkSymlink; };
 
-      		  if [ ! -d "${dotfiles_path}/.git" ]; then
-      				echo "--- Cloning Dotfiles dari GitHub msalmanrafadhlih/racooonfig/${branch} ---"
-      				${pkgs.git}/bin/git clone -b ${branch} --single-branch --depth 1 ${repo_url} "${dotfiles_path}"
-      		  else
-      				echo "--- Dotfiles sudah ada, melakukan check update (safe) ---"
-      				cd "${dotfiles_path}"
+    home.activation = {
+      setupDotfiles = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        		  # Buat folder .dotfiles jika belum ada
+        		  mkdir -p ${dotfiles_path}
 
-      				# Melakukan fetch saja agar tidak merusak kerjaan lokal
-              if ${pkgs.git}/bin/git ls-remote ${repo_url} &>/dev/null; then
-                ${pkgs.git}/bin/git fetch origin ${branch}
-              else
-                echo "offline, skip"
-              fi
-      				echo "Gunakan 'git pull --rebase' secara manual di folder dotfiles jika ingin sinkronisasi."
-      		  fi
-      		'';
+        		  if [ ! -d "${dotfiles_path}/.git" ]; then
+        				echo "--- Cloning Dotfiles dari GitHub msalmanrafadhlih/racooonfig/${branch} ---"
+        				${pkgs.git}/bin/git clone -b ${branch} --single-branch --depth 1 ${repo_url} "${dotfiles_path}"
+        		  else
+        				echo "--- Dotfiles sudah ada, melakukan check update (safe) ---"
+        				cd "${dotfiles_path}"
+
+        				# Melakukan fetch saja agar tidak merusak kerjaan lokal
+                if ${pkgs.git}/bin/git ls-remote ${repo_url} &>/dev/null; then
+                  ${pkgs.git}/bin/git fetch origin ${branch}
+                else
+                  echo "offline, skip"
+                fi
+        				echo "Gunakan 'git pull --rebase' secara manual di folder dotfiles jika ingin sinkronisasi."
+        		  fi
+        		'';
+    };
   };
 }
