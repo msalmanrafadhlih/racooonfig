2e744c1ea7c43ac7be9c0481acefd64dbcf41c64
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/bspwm/gui-apps.nix

--- modules/homeModules/bspwm/gui-apps.nix
+++ modules/homeModules/bspwm/gui-apps.nix
@@ -1,39 +1,48 @@
-{ pkgs, inputs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (cfg.homeManager && builtins.elem "bspwm" cfg.listConfigurations) {
+    # GUI Apps
+    programs = {
+      obs-studio.enable = true;
+    };
 
-  # GUI Apps
-  programs = {
-    obs-studio.enable = true;
+    home.packages = with pkgs; [
+      ungoogled-chromium
+      vivaldi
+      xarchiver
+      gparted # Manage Disk Partition
+      vesktop
+      ulauncher
+      pavucontrol
+      # font-manager
+      # picard
+      # protonvpn-gui
+      # qbittorrent
+
+      # Editor # Productivity
+      zoom
+      libreoffice
+      evince # Document Viewer
+      kdePackages.kdenlive
+      audacity
+      gimp
+      gthumb
+      # godot
+
+      # CLI TOOLS
+      ani-cli # butuh mpv(GUI) untuk streaming
+      go-grip # Markdown Preview via http server: butuh browser
+
+      # UTILS
+      ffmpeg-full # absolutely need GUI
+    ];
   };
-
-  home.packages = with pkgs; [
-    ungoogled-chromium
-    vivaldi
-    xarchiver
-    gparted # Manage Disk Partition
-    vesktop
-    ulauncher
-    pavucontrol
-    # font-manager
-    # picard
-    # protonvpn-gui
-    # qbittorrent
-
-    # Editor # Productivity
-    zoom
-    libreoffice
-    evince # Document Viewer
-    kdePackages.kdenlive
-    audacity
-    gimp
-    gthumb
-    # godot
-
-    # CLI TOOLS
-    ani-cli # butuh mpv(GUI) untuk streaming
-    go-grip # Markdown Preview via http server: butuh browser
-
-    # UTILS
-    ffmpeg-full # absolutely need GUI
-  ];
 }
