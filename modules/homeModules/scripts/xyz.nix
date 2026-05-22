7f11ea66f66574b0d2b69e9fb10c297e04184490
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/scripts/xyz.nix

--- modules/homeModules/scripts/xyz.nix
+++ modules/homeModules/scripts/xyz.nix
@@ -1,22 +1,25 @@
+{ config, lib, ... }:
 {
-  home.file.".local/bin/xyz" = {
-    text = ''
-	    #!/usr/bin/env bash
+  config = lib.mkIf config.racooonfig.homeManager {
+    home.file.".local/bin/xyz" = {
+      text = ''
+        	    #!/usr/bin/env bash
 
-		# ambil resolusi layar utama (X11 dengan xrandr)
-		RES=$(xrandr | grep '*' | awk '{print $1}' | head -n1)
-		SCREEN_W=''${RES%x*}
-		SCREEN_H=''${RES#*x}
+        		# ambil resolusi layar utama (X11 dengan xrandr)
+        		RES=$(xrandr | grep '*' | awk '{print $1}' | head -n1)
+        		SCREEN_W=''${RES%x*}
+        		SCREEN_H=''${RES#*x}
 
-		read -p "lebar? " WIN_W
-		read -p "tinggi? " WIN_H
+        		read -p "lebar? " WIN_W
+        		read -p "tinggi? " WIN_H
 
-		# hitung koordinat kiri-atas agar window tepat di tengah
-		X=$(( (SCREEN_W - WIN_W) / 2 ))
-		Y=$(( (SCREEN_H - WIN_H) / 2 ))
+        		# hitung koordinat kiri-atas agar window tepat di tengah
+        		X=$(( (SCREEN_W - WIN_W) / 2 ))
+        		Y=$(( (SCREEN_H - WIN_H) / 2 ))
 
-		echo "rectangle=''${WIN_W}x''${WIN_H}+''${X}+''${Y}"
-    '';
-    executable = true;
+        		echo "rectangle=''${WIN_W}x''${WIN_H}+''${X}+''${Y}"
+      '';
+      executable = true;
+    };
   };
 }
