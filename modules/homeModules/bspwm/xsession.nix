f9c349b745faaf01e009c951da5ceab842627c58
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/homeModules/bspwm/xsession.nix

--- modules/homeModules/bspwm/xsession.nix
+++ modules/homeModules/bspwm/xsession.nix
@@ -1,30 +1,37 @@
 # ./modules/xsession.nix
+{ config, lib, ... }:
+let
+  cfg = config.racooonfig;
+in
 {
-  home.sessionVariables = {
-    BROWSER = "vivaldi";
-    TERMINAL = "st";
-    XCURSOR_SIZE = "24";
+  config = lib.mkIf (cfg.homeManager && builtins.elem "bspwm" cfg.listConfigurations) {
+    home.sessionVariables = {
+      BROWSER = "vivaldi";
+      TERMINAL = "st";
+      XCURSOR_SIZE = "24";
 
-    QT_QPA_PLATFORMTHEME = "qt5ct";
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+    };
+
+    xsession = {
+      enable = true;
+      windowManager.command = "bspwm";
+
+      # Tambahan script sebelum menjalankan WM (setara isi .xinitrc)
+      initExtra = ''
+            [[ -f ~/.Xresources ]] && xrdb -merge ~/.Xresources
+            xsetroot -cursor_name left_ptr	    
+
+            if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+                eval "$(dbus-launch --sh-syntax --exit-with-session)"
+            fi
+
+        		mpDris2 &
+
+        		# disable keyboard internal
+            xinput disable "AT Translated Set 2 keyboard"
+      '';
+    };
   };
 
-  xsession = {
-    enable = true;
-    windowManager.command = "bspwm";
-
-    # Tambahan script sebelum menjalankan WM (setara isi .xinitrc)
-    initExtra = ''
-      [[ -f ~/.Xresources ]] && xrdb -merge ~/.Xresources
-      xsetroot -cursor_name left_ptr	    
-
-      if [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-          eval "$(dbus-launch --sh-syntax --exit-with-session)"
-      fi
-
-  		mpDris2 &
-
-  		# disable keyboard internal
-      xinput disable "AT Translated Set 2 keyboard"
-    '';
-  };
 }
