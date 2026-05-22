7985e9da6b594b56d850cfe3a71930fc03ac95f3
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 14:36:29 +0700

    2026-05-22 14:36 | Update via SAVEFLAKE: modules/nixosModules/bspwm/services.nix

--- modules/nixosModules/bspwm/services.nix
+++ modules/nixosModules/bspwm/services.nix
@@ -1,31 +1,40 @@
-{ inputs, pkgs, lib, ... }:
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
 let
   inp = inputs.racooonfig.inputs;
+  cfg = config.racooonfig;
 in
 {
   imports = [ inp.nix-flatpak.nixosModules.nix-flatpak ];
-  security.pam.services.i3lock.enable = true;
-  security.pam.services.i3lock = { };
-  security.pam.services.i3lock.text = ''
-    		auth include login
-    	'';
-  services = {
-    # install flatpak binary
-    # nix-snapd = true;
-    flatpak = {
-      enable = true;
-      uninstallUnmanaged = true;
+  config = lib.mkIf (cfg.enable && builtins.elem "bspwm" cfg.windowManager) {
+    security.pam.services.i3lock.enable = true;
+    security.pam.services.i3lock = { };
+    security.pam.services.i3lock.text = ''
+      		auth include login
+      	'';
+    services = {
+      # install flatpak binary
+      # nix-snapd = true;
+      flatpak = {
+        enable = true;
+        uninstallUnmanaged = true;
+      };
+      ###################################
+      ## THUNAR OPTIMALIZATION
+      #####################################
+      tumbler.enable = lib.mkDefault true; # thumbnails di Thunar
+      udisks2.enable = lib.mkDefault true; # Storage Manager (USBmounting, external HDD/SSD)
+      dbus.enable = lib.mkDefault true; # App to App Communications (Discord RPC, MountUSB in FileManager)
+      gvfs.enable = lib.mkDefault true; # Thunar integration with plugins
     };
-    ###################################
-    ## THUNAR OPTIMALIZATION
-    #####################################
-    tumbler.enable = lib.mkDefault true; # thumbnails di Thunar
-    udisks2.enable = lib.mkDefault true; # Storage Manager (USBmounting, external HDD/SSD)
-    dbus.enable = lib.mkDefault true; # App to App Communications (Discord RPC, MountUSB in FileManager)
-    gvfs.enable = lib.mkDefault true; # Thunar integration with plugins
-  };
 
-  programs.gdk-pixbuf.modulePackages = [
-    pkgs.webp-pixbuf-loader
-  ];
+    programs.gdk-pixbuf.modulePackages = [
+      pkgs.webp-pixbuf-loader
+    ];
+  };
 }
