778e6b22c39a9cf39af018438ed9d56ebf8b412d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 19:15:14 +0700

    2026-05-22 19:15 | Update via SAVEFLAKE: configs/alacritty/default.nix

--- configs/alacritty/default.nix
+++ configs/alacritty/default.nix
@@ -1,14 +1,24 @@
 {
+  mkSymlink,
   pkgs,
   config,
   lib,
   ...
 }:
 let
+  configs = {
+    "alacritty/alacritty.toml" = "alacritty.toml";
+    "alacritty/fonst.toml" = "fonts.toml";
+  };
   cfg = config.racooonfig;
 in
 {
+
   config = lib.mkIf (cfg.homeManager && builtins.elem "alacritty" cfg.listConfigurations) {
+    xdg.configFile = mkSymlink {
+      target = "alacritty";
+    } configs;
+
     home.packages = with pkgs; [
       alacritty
     ];
