5ebc5eedbb7984fcddf8153a74e1fb5d501bfc26
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-22 19:15:14 +0700

    2026-05-22 19:15 | Update via SAVEFLAKE

---
+++ modules/nixosModules/bspwm/bspwm.nix
@@ -0,0 +1,30 @@
+{ lib, config, ... }:
+let
+  cfg = config.racooonfig;
+in
+
+{
+  config = lib.mkIf (cfg.enable && builtins.elem "bspwm" cfg.windowManager) {
+    services = {
+      xserver = {
+        enable = true;
+        windowManager = {
+          bspwm.enable = true;
+        };
+        autoRepeatDelay = 300;
+        autoRepeatInterval = 35;
+        displayManager = {
+          startx.enable = false; # disable if set lightdm to true
+          # lightdm
+          lightdm = {
+            enable = false;
+            background = builtins.fetchurl {
+              url = "https://raw.githubusercontent.com/msalmanrafadhlih/Nixos-Dotsfile/refs/heads/main/config/Assets/Wallpaper/wallpaper8.jpeg";
+              sha256 = "sha256-VZp1wy2N0GApt48ILRY+pIAhAjCt02GmqmxHRTWAEoA=";
+            };
+          };
+        };
+      };
+    };
+  };
+}
