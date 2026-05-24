aaa5a1b959d514d6ed15bbc77f764524202f494b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:44 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: configs/code/default.nix

--- configs/code/default.nix
+++ configs/code/default.nix
@@ -1,13 +1,5 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  cfg = config.racooonfig;
-in
-{
+{ config, lib, pkgs, ... }: let cfg = config.racooonfig;
+in {
   config = lib.mkIf (cfg.homeManager && builtins.elem "vscode" cfg.listConfigurations) {
     home.packages = with pkgs; [
       vscode
