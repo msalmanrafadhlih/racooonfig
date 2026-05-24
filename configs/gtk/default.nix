524016a5c6dc63fe217acb02e7cf7555cabec7a0
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:44 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: configs/gtk/default.nix

--- configs/gtk/default.nix
+++ configs/gtk/default.nix
@@ -1,41 +1,13 @@
-{
-  pkgs,
-  config,
-  ...
-}:
-let
-  HOME = config.home.homeDirectory;
+{ pkgs, config, lib, ... }: let
+  cfg = config.racooonfig;
 in
 {
-  home.packages = with pkgs; [
-    vimix-icon-theme
-    cursor-memes
-    vimix-gtk-themes
-  ];
-
-  # Copy/symlink theme ke ~/.themes
-  home.file.".themes/dynamic/gtk-4.0/assets".source = ./.themes/gtk-4.0/assets;
-  home.file.".themes/dynamic/gtk-4.0/libadwaita.css".source = ./.themes/gtk-4.0/libadwaita.css;
-  home.file.".themes/dynamic/gtk-4.0/libadwaita-tweaks.css".source =
-    ./.themes/gtk-4.0/libadwaita-tweaks.css;
-
-  home.file.".themes/dynamic/gtk-3.0".source = ./.themes/gtk-3.0;
-  home.file.".themes/dynamic/index.theme".source = ./.themes/index.theme;
-
-  # gtk.css
-  home.file.".themes/dynamic/gtk-4.0/gtk.css".text = ''
-    @import url("file://${HOME}/.config/gtk-4.0/gtk.css");
-    @import url("libadwaita.css");
-    @import url("libadwaita-tweaks.css");
-  '';
-
-  home.file.".themes/dynamic/gtk-4.0/gtk-dark.css".text = ''
-    @import url("file://${HOME}/.config/gtk-4.0/colors.css");
-    @import url("libadwaita.css");
-    @import url("libadwaita-tweaks.css");
-  '';
-
-  imports = [
-    ./gtkrc.nix
-  ];
+  config = lib.mkIf cfg.homeManager {
+    home.file = import ./gtkrc.nix { inherit config; };
+    home.packages = with pkgs; [
+      vimix-gtk-themes
+      vimix-icon-theme
+      cursor-memes
+    ];
+  };
 }
