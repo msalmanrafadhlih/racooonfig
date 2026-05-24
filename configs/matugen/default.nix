2e564ff2f376b8be3c434ca9d1b5913b03268c69
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:44 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: configs/matugen/default.nix

--- configs/matugen/default.nix
+++ configs/matugen/default.nix
@@ -1,15 +1,17 @@
-{ mkSymlink, ... }:
+{ mkSymlink, lib, config, ... }:
 let
+  cfg = config.racooonfig;
   configs = {
-		"matugen/templates"   = "templates";
-		"matugen/websites"    = "websites";
-		"matugen/config.toml" = "config.toml";
+    "matugen/templates" = "templates";
+    "matugen/websites" = "websites";
+    "matugen/config.toml" = "config.toml";
   };
-
 in
 {
-  # Symlink path to ~./config/*
-  xdg.configFile = mkSymlink {
-    target = "matugen";
-  } configs;
+  config = lib.mkIf cfg.homeManager {
+    # Symlink path to ~./config/*
+    xdg.configFile = mkSymlink {
+      target = "matugen";
+    } configs;
+  };
 }
