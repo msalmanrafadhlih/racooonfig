de9d10badca3144508d20fcff611e8d32f834987
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:45 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: modules/homeModules/bspwm/xsession.nix

--- modules/homeModules/bspwm/xsession.nix
+++ modules/homeModules/bspwm/xsession.nix
@@ -1,10 +1,13 @@
 # ./modules/xsession.nix
-{ config, lib, ... }:
+{ config, lib, mkSymlink, ... }:
 let
   cfg = config.racooonfig;
 in
 {
   config = lib.mkIf (cfg.homeManager && builtins.elem "bspwm" cfg.listConfigurations) {
+
+    xdg = import ../../../configs/bspwm { inherit mkSymlink; };
+
     home.sessionVariables = {
       BROWSER = "vivaldi";
       TERMINAL = "st";
