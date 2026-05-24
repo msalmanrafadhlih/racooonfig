782742c06dbc7bafc452e71b364e3f527b366498
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:45 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: modules/homeModules/niri/default.nix

--- modules/homeModules/niri/default.nix
+++ modules/homeModules/niri/default.nix
@@ -1,1 +1,19 @@
-{ ... }: { }
+# ./modules/xsession.nix
+{ config, lib, mkSymlink, ... }:
+let
+  cfg = config.racooonfig;
+in
+{
+  config = lib.mkIf (cfg.homeManager && builtins.elem "niri" cfg.listConfigurations) {
+
+    # xdg = import ../../../configs/niri { inherit mkSymlink; };
+
+    home.sessionVariables = {
+      BROWSER = "vivaldi";
+      TERMINAL = "st";
+      XCURSOR_SIZE = "24";
+
+      QT_QPA_PLATFORMTHEME = "qt5ct";
+    };
+  };
+}
