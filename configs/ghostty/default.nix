e5b455e7a38ecdbfef882254945e589b0f556a7e
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:44 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: configs/ghostty/default.nix

--- configs/ghostty/default.nix
+++ configs/ghostty/default.nix
@@ -1,14 +1,5 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-
-let
-  cfg = config.racooonfig;
-in
-{
+{ config, lib, pkgs, ... }: let cfg = config.racooonfig;
+in {
   config = lib.mkIf (cfg.homeManager && builtins.elem "ghostty" cfg.listConfigurations) {
     home.packages = with pkgs; [
       ghostty
