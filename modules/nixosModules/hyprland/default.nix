aad3a7f447876df50c9c4c5e412cd1799206a01b
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-24 16:13:45 +0700

    2026-05-24 16:13 | Update via SAVEFLAKE: modules/nixosModules/hyprland/default.nix

--- modules/nixosModules/hyprland/default.nix
+++ modules/nixosModules/hyprland/default.nix
@@ -1,1 +1,12 @@
-{ ... }: { }
+{ lib, config, pkgs, ... }:
+let
+  cfg = config.racooonfig;
+in
+
+{
+  config = lib.mkIf (cfg.enable && builtins.elem "hyprland" cfg.windowManager) {
+    environment.systemPackages = with pkgs; [
+      hyprland
+    ];
+  };
+}
