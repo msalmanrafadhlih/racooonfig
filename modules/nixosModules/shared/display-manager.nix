b4ca52906bdf350e5d7fd8bec9faf1cf07fb3a66
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-25 20:00:40 +0700

    2026-05-25 20:00 | Update via SAVEFLAKE: modules/nixosModules/shared/display-manager.nix

--- modules/nixosModules/shared/display-manager.nix
+++ modules/nixosModules/shared/display-manager.nix
@@ -19,6 +19,7 @@ in
           displayManager = {
             sddm = {
               enable = lib.mkDefault true;
+              wayland.enable =  true;
               theme = lib.mkDefault "orbital";
               setupScript = ''
                 ${pkgs.xrdb}/bin/xrdb -merge - <<EOF
