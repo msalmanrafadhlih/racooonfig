cb22d310909a26d114d60012f9db174d2f776058
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-26 16:49:08 +0700

    2026-05-26 16:49 | Update via SAVEFLAKE: configs/hyprland/default.nix

--- configs/hyprland/default.nix
+++ configs/hyprland/default.nix
@@ -8,10 +8,9 @@ let
   configs = {
     "Hyprland/rices"     = "rices";
     "hypr/config"        = "config";
-    "hypr/scripts"       = "scripts";
     "hypr/.luarc.json"   = ".luarc.json";
     "hypr/hyprland.lua"  = "hyprland.lua";
-    "hyhl.meta.lua"      = "hl.meta.lua";
+    "hypr/hl.meta.lua"   = "hl.meta.lua";
   };
 in
 {
