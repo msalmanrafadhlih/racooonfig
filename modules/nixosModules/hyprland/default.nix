c700734ed99b081c2f8d66c3366d09fe2b531125
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-27 01:34:49 +0700

    2026-05-27 01:34 | Update via SAVEFLAKE: modules/nixosModules/hyprland/default.nix

--- modules/nixosModules/hyprland/default.nix
+++ modules/nixosModules/hyprland/default.nix
@@ -11,9 +11,6 @@ in
   config = lib.mkIf (cfg.enable && builtins.elem "hyprland" cfg.windowManager) {
     programs.hyprland = {
       enable = true;
-      package = inp.hyprland.packages.${system}.hyprland;
-      portalPackage = inp.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
-      withUWSM = true;
     };
 
     hardware.graphics = {
