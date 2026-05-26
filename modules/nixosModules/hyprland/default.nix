c861c0059efec9f91f0ffadd4bba3e65346d5d90
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-26 15:05:33 +0700

    2026-05-26 15:05 | Update via SAVEFLAKE: modules/nixosModules/hyprland/default.nix

--- modules/nixosModules/hyprland/default.nix
+++ modules/nixosModules/hyprland/default.nix
@@ -1,11 +1,34 @@
-{ lib, config, ... }:
+{ lib, config, inputs, pkgs, ... }:
 let
+  system = pkgs.stdenv.hostPlatform.system;
+  inp = inputs.racooonfig.inputs;
   cfg = config.racooonfig;
+
+  hypr-unstable = inp.hyprland.inputs.nixpkgs.legacyPackages.${system};
 in
 
 {
   config = lib.mkIf (cfg.enable && builtins.elem "hyprland" cfg.windowManager) {
-    programs.hyprland.enable = true;
+    programs.hyprland = {
+      enable = true;
+      package = inp.hyprland.packages.${system}.hyprland;
+      portalPackage = inp.hyprland.packages.${system}.xdg-desktop-portal-hyprland;
+    };
+
+    hardware.graphics = {
+      package = hypr-unstable.mesa;
+
+      # 32-bit support (e.g for Steam)
+      enable32Bit = lib.mkForce true;
+      package32 = hypr-unstable.pkgsi686Linux.mesa;
+    };
+
     services.pipewire.enable = true;
+
+    nix.settings = {
+      substituters         = lib.mkAfter [ "https://hyprland.cachix.org" ];
+      trusted-substituters = lib.mkAfter [ "https://hyprland.cachix.org" ];
+      trusted-public-keys  = lib.mkAfter [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    };
   };
 }
