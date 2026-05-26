9783f6c0620b87915e200714d12f0c272ad8c016
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-26 15:05:33 +0700

    2026-05-26 15:05 | Update via SAVEFLAKE: flake.nix

--- flake.nix
+++ flake.nix
@@ -55,11 +55,13 @@
     };
 
     ## ---- Home-Manager Packages
+    hyprland.url = "github:hyprwm/Hyprland";
+
     zjstatus = {
       url = "github:dj95/zjstatus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
+    
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
