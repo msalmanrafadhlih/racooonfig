33eb7b2a4a129c5883bd10390ddc8846fdbe3c91
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-26 22:52:59 +0700

    2026-05-26 22:52 | Update via SAVEFLAKE: configs/hyprland/config/settings.lua

--- configs/hyprland/config/settings.lua
+++ configs/hyprland/config/settings.lua
@@ -4,55 +4,189 @@
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 
 hl.config({
+	-- https://wiki.hypr.land/Configuring/Basics/Variables/#general
 	general = {
-		gaps_in = 4,
-		gaps_out = 4,
-
-
-		col = {
-			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
-			inactive_border = "rgba(595959aa)",
-		},
+		layout = "scrolling", -- dwindle. master, scrolling, monocle
+		locale = "en_US",
 
 		-- Set to true to enable resizing windows by clicking and dragging on borders and gaps
 		border_size = 2,
 		resize_on_border = true,
-    extend_border_grab_area = 30,
+    extend_border_grab_area = 15,
+    hover_icon_on_border = true,
+		gaps_in = 4,
+		gaps_out = 4,
+		gaps_workspaces = 1,
+		float_gaps = 1,
+
+		col = {
+			active_border = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
+			inactive_border = "rgba(595959aa)",
+			nogroup_border = "0xffffaaff",
+			nogroup_border_active = "0xffff00ff",
+		},
 
 		-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
 		allow_tearing = true,
-
-		layout = "dwindle",
+		resize_corner = true,
+		modal_parent_blocking = true,
+		no_focus_fallback = false,
+		snap = {
+			enabled = true,
+			window_gap = 10,
+			monitor_gap = 10,
+			border_overlap = true,
+			respect_gaps = true,
+		}
 	},
 
+	-- https://wiki.hypr.land/Configuring/Basics/Variables/#decoration
 	decoration = {
 		rounding = 10,
-		rounding_power = 2,
-
+		rounding_power = 2.0,
 		-- Change transparency of focused and unfocused windows
 		active_opacity = 1.0,
 		inactive_opacity = 1.0,
-
+		fullscreen_opacity = 1.0;
+		dim_modal = true,
+		dim_inactive = false,
+		dim_strength = 0.8,
+		dim_special = 0.2,
+		dim_around = 0.4,
+		screen_shader = "",
+		border_part_of_window = true,
 		shadow = {
 			enabled = false,
 			range = 4,
 			render_power = 3,
 			color = 0xee1a1a1a,
+			color_inactive = nil,
+			sharp = false,
+			offset = { 0, 0 },
+			scale = 1.0,
+		},
+
+		glow = {
+			enabled = false,
+			range = 10,
+			render_power = 3,
+			color = 0xee1a1a1a,
+			color_inactive = nil,
 		},
 
 		blur = {
 			enabled = true,
 			size = 8,
 			passes = 2,
+			special = false,
+			popups = false,
+			popups_ignorealpha = 0.2,
 			vibrancy = 0.1696,
+			vibrancy_darkness = 0.0,
+			xray = false,
+			noise = 0.0117,
+			contrast = 0.8916,
+			brightness = 0.8172,
+			input_methods = false,
+			input_methods_ignorealpha = 0.2,
+			ignore_opacity = true,
+			new_optimizations = true,
 		},
 	},
 
+	misc = {
+		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
+	},
+
+	-- https://wiki.hypr.land/Configuring/Basics/Variables/#input
+	input = {
+		kb_model = "",
+		kb_layout = "us",
+		kb_variant = "",
+		kb_options = "grp:alt_shift_toggle",
+		kb_rules = "",
+		kb_file = "",
+    repeat_rate = 40,
+    repeat_delay = 250,
+		numlock_by_default = "",
+		resolve_binds_by_sym = false,
+
+		follow_mouse = 1,
+		accel_profile = "adaptive", -- flat, adaptive, custom
+		force_no_accel = false,
+		rotation = 0,
+		left_handed = false,
+		scroll_points = nil,
+		scroll_method = nil, -- 2fg, edge, on_button_down, no_scroll
+		scroll_button = 0,
+		scroll_button_lock = false,
+		scroll_factor = 1.0,
+		natural_scroll = false,
+		sensitivity = 0.0, -- -1.0 - 1.0, 0 means no modification.
+		follow_mouse_shrink = 0,
+		follow_mouse_threshold = 0.0,
+		focus_on_close = 2,
+		mouse_refocus = true,
+		float_switch_override_focus = 1,
+		special_fallthrough = false,
+		emulate_discrete_scroll = 1,
+		off_window_axis_events = 1,
+
+		-- https://wiki.hypr.land/Configuring/Basics/Variables/#touchpad
+		touchpad = {
+			disable_while_typing = true,
+			natural_scroll = true,
+			scroll_factor = 1.0,
+			middle_button_emulation = false,
+			tap_button_map = nil, -- lrm, lmr
+			clickfinger_behavior = false,
+			tap_to_click = true,
+			drag_lock = 0,
+			tap_and_drag = true,
+			flip_x = false,
+			flip_y = false,
+			drag_3fg = 0,
+		},
+
+		-- https://wiki.hypr.land/Configuring/Basics/Variables/#touchdevice
+		touchdevice = {
+			transform = -1,
+			-- output = "",
+			enabled = true,
+		},
+
+		-- https://wiki.hypr.land/Configuring/Basics/Variables/#virtualkeyboard
+		virtualkeyboard = {
+			share_state = 2,
+			release_pressed_on_close = false,
+		},
+
+		tablet = {}, -- https://wiki.hypr.land/Configuring/Basics/Variables/#tablet
+		tablettool = {}, -- https://wiki.hypr.land/Configuring/Basics/Variables/#tablettool
+
+	},
+
+	-- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
+	dwindle = {
+    -- pseudotile = true, -- Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
+		preserve_split = true, -- You probably want this
+	},
+
+	-- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
+	master = {
+		new_status = "master",
+	},
+
+	-- https://wiki.hypr.land/Configuring/Basics/Variables/#animations
 	animations = {
 		enabled = true,
+		workspace_wraparound = false,
 	},
 })
 
+-- Default springs
+hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 -- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
 hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
 hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
@@ -60,8 +194,6 @@ hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
 hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
 hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
 
--- Default springs
-hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
 
 hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
@@ -81,205 +213,9 @@ hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "al
 hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
 hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
 
--- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
--- "Smart gaps" / "No gaps when only"
--- uncomment all if you wish to use that.
--- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
--- hl.workspace_rule({ workspace = "f[1]",   gaps_out = 0, gaps_in = 0 })
--- hl.window_rule({
---     name  = "no-gaps-wtv1",
---     match = { float = false, workspace = "w[tv1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
--- hl.window_rule({
---     name  = "no-gaps-f1",
---     match = { float = false, workspace = "f[1]" },
---     border_size = 0,
---     rounding    = 0,
--- })
-
--- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
-hl.config({
-	dwindle = {
-    -- pseudotile = true, -- Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
-		preserve_split = true, -- You probably want this
-	},
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
-hl.config({
-	master = {
-		new_status = "master",
-	},
-})
-
--- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
-hl.config({
-	scrolling = {
-		fullscreen_on_one_column = true,
-	},
-})
-
-----------------
-----  MISC  ----
-----------------
-
-hl.config({
-	misc = {
-		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
-		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-	},
-})
-
----------------
----- INPUT ----
----------------
-
-hl.config({
-	input = {
-		kb_layout = "us",
-		kb_variant = "",
-		kb_model = "",
-		kb_options = "grp:alt_shift_toggle",
-		kb_rules = "",
-		accel_profile = "flat",
-
-    -- Mengatur jeda awal sebelum karakter berulang (dalam milidetik)
-    -- Nilai 250 - 300 ms adalah angka yang ideal dan responsif
-    repeat_delay = 250,
-
-    --  Mengatur berapa banyak karakter yang keluar per detik saat tombol ditahan
-    --  Nilai 35 - 40 membuat penghapusan teks atau pengetikan berulang sangat cepat
-    repeat_rate = 40,
-		follow_mouse = 1,
-
-		sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
-
-		touchpad = {
-			natural_scroll = true
-		},
-	},
-})
 
 hl.gesture({
 	fingers = 3,
 	direction = "horizontal",
 	action = "workspace",
 })
-
--- Example per-device config
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
-hl.device({
-	name = "epic-mouse-v1",
-	sensitivity = -0.5,
-})
-
-
--- general {
---     border_size = 2
---     gaps_in = 4
---     gaps_out = 4
---     float_gaps = 6
-
---     # Set to true enable resizing windows by clicking and dragging on borders and gaps
---     resize_on_border = true
---     extend_border_grab_area = 30
-
---     col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
---     col.inactive_border = rgba(595959aa)
-
---     # col.active_border = $active_border
---     # col.inactive_border = $inactive_border
-
---     # Please see https://wiki.hypr.land/Configuring/Tearing/ before you turn this on
---     allow_tearing = true
---     layout = dwindle
--- }
-
--- decoration {
---     rounding = 3
---     rounding_power = 2
-
---     active_opacity = 1.0
---     inactive_opacity = 1.0
-
---     blur {
---         enabled = true
---         size = 8
---         passes = 2
---         new_optimizations = true
-
---         vibrancy = 0.1696
---     }
---     shadow {
---         enabled = false
---         range = 4
---         render_power = 3
---         color = rgba(1a1a1aee)
---     }
--- }
-
--- input {
---     kb_layout = us, ru
---     kb_options = grp:alt_shift_toggle
---     kb_variant = 
---     kb_model = 
---     kb_rules = 
---     accel_profile = flat
-
---     # Mengatur jeda awal sebelum karakter berulang (dalam milidetik)
---     # Nilai 250 - 300 ms adalah angka yang ideal dan responsif
---     repeat_delay = 250
-
---     # Mengatur berapa banyak karakter yang keluar per detik saat tombol ditahan
---     # Nilai 35 - 40 membuat penghapusan teks atau pengetikan berulang sangat cepat
---     repeat_rate = 40
-
---     follow_mouse = 1
-
---     sensitivity = 0.5 # -1.0 - 1.0, 0 means no modification.
-
---     touchpad {
---         natural_scroll = true
---     }
--- }
-
--- misc {
---     force_default_wallpaper = -1 # Set to 0 or 1 to disable the anime mascot wallpapers
---     focus_on_activate = true
---     font_family = JetBrains Mono
---     disable_hyprland_logo = false
---     disable_splash_rendering = true
--- }
-
--- # See https://wiki.hypr.land/Configuring/Master-Layout/ for more
--- master {
---     new_status = master
--- }
-
--- dwindle {
---     pseudotile = true # Master switch for pseudotiling. Enabling is bound to mainMod + P in the keybinds section below
---     preserve_split = true # You probably want this
--- }
-
--- animations {
---     enabled = yes
---     bezier = myBezier, 0.05, 0.9, 0.1, 1.05
---     animation = windows, 1, 5, myBezier, popin 80%
---     animation = windowsOut, 1, 5, myBezier, popin 80%
---     animation = layers, 1, 5, myBezier, fade
---     animation = layersIn, 1, 5, myBezier, fade
---     animation = layersOut, 1, 5, myBezier, fade
---     animation = fade, 1, 5, myBezier
---     animation = workspaces, 1, 5, myBezier, slide
---     animation = specialWorkspaceIn, 1, 5, myBezier, fade
---     animation = specialWorkspaceOut, 1, 5, myBezier, fade
--- }
-
--- # Example per-device config
--- # See https://wiki.hypr.land/Configuring/Keywords/#per-device-input-configs for more
--- device {
---     name = epic-mouse-v1
---     sensitivity = -0.5
--- }
