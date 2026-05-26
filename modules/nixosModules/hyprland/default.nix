f720589edfea6c7bec70f2aa01cd3a8ec6e9f02c
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-27 03:21:01 +0700

    2026-05-27 03:21 | Update via SAVEFLAKE: modules/nixosModules/hyprland/default.nix

--- modules/nixosModules/hyprland/default.nix
+++ modules/nixosModules/hyprland/default.nix
@@ -8,6 +8,7 @@ let
 in
 
 {
+
   config = lib.mkIf (cfg.enable && builtins.elem "hyprland" cfg.windowManager) {
     programs.hyprland = {
       enable = true;
@@ -21,12 +22,12 @@ in
       # 32-bit support (e.g for Steam)
       enable32Bit = lib.mkForce true;
       package32 = hypr-unstable.pkgsi686Linux.mesa;
+    };
 
-      nix.settings = {
-        substituters         = lib.mkAfter [ "https://hyprland.cachix.org" ];
-        trusted-substituters = lib.mkAfter [ "https://hyprland.cachix.org" ];
-        trusted-public-keys  = lib.mkAfter [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
-      };
+    nix.settings = {
+      substituters         = lib.mkAfter [ "https://hyprland.cachix.org" ];
+      trusted-substituters = lib.mkAfter [ "https://hyprland.cachix.org" ];
+      trusted-public-keys  = lib.mkAfter [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
     };
   };
 }
