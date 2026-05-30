66f93e82692273c43e257d126540c4891f6e327a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-05-30 16:49:38 +0700

    Update via SAVEFLAKE - 2026-05-30 16:49
    Update file: configs/bspwm/default.nix
    
    Changes:
    -    polybar = "polybar";
    -    picom = "picom";
    -    sxhkd = "sxhkd";
    -    dunst = "dunst";
    -    rofi = "rofi";
    -    eww = "eww";
    -    "bspwm/bin" = "bin";
    -    "bspwm/rices" = "rices";
    -    "bspwm/.term" = "var/.term";
    -    "bspwm/.rice" = "var/.rice";
    +

--- configs/bspwm/default.nix
+++ configs/bspwm/default.nix
@@ -7,18 +7,19 @@
 let
 
   configs = {
-    polybar = "polybar";
-    picom = "picom";
-    sxhkd = "sxhkd";
-    dunst = "dunst";
-    rofi = "rofi";
-    eww = "eww";
+    polybar         = "polybar";
+    picom           = "picom";
+    sxhkd           = "sxhkd";
+    dunst           = "dunst";
+    rofi            = "rofi";
+    eww             = "eww";
 
-    "bspwm/bin" = "bin";
-    "bspwm/rices" = "rices";
+    "bspwm/bin"     = "bin";
+    "bspwm/rices"   = "rices";
     "bspwm/bspwmrc" = "bspwmrc";
-    "bspwm/.term" = "var/.term";
-    "bspwm/.rice" = "var/.rice";
+
+    "bspwm/.term"   = "var/.term";
+    "bspwm/.rice"   = "var/.rice";
   };
 in
 {
