f39ffb9c1ed6584d0ecb2f2799cc5bc333f1d202
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-02 15:43:35 +0700

    Update via SAVEFLAKE - 2026-06-02 15:43
    Update file: modules/nixosModules/shared/services.nix
    
    Changes:
    -
    -      power-profiles-daemon.enable = true;
    -      ###################################
    -      ## THUNAR OPTIMALIZATION
    -      #####################################
    -      tumbler.enable = lib.mkDefault true; # thumbnails di Thunar
    -      udisks2.enable = lib.mkDefault true; # Storage Manager (USBmounting, external HDD/SSD)
    -      dbus.enable = lib.mkDefault true; # App to App Communications (Discord RPC, MountUSB in FileManager)
    -      gvfs.enable = lib.mkDefault true; # Thunar integration with plugins
    -

--- modules/nixosModules/shared/services.nix
+++ modules/nixosModules/shared/services.nix
@@ -13,6 +13,8 @@ in
   imports = [ inp.nix-flatpak.nixosModules.nix-flatpak ];
   config = lib.mkIf cfg.enable {
     services = {
+      udisks2.enable = lib.mkDefault true; # Storage Manager (USBmounting, external HDD/SSD)
+      dbus.enable = lib.mkDefault true; # App to App Communications (Discord RPC, MountUSB in FileManager)
       pipewire.enable = true;
       # install flatpak binary
       # nix-snapd = true;
@@ -20,17 +22,7 @@ in
         enable = lib.mkDefault true;
         uninstallUnmanaged = lib.mkDefault true;
       };
-
-      power-profiles-daemon.enable = true;
-      ###################################
-      ## THUNAR OPTIMALIZATION
-      #####################################
-      tumbler.enable = lib.mkDefault true; # thumbnails di Thunar
-      udisks2.enable = lib.mkDefault true; # Storage Manager (USBmounting, external HDD/SSD)
-      dbus.enable = lib.mkDefault true; # App to App Communications (Discord RPC, MountUSB in FileManager)
-      gvfs.enable = lib.mkDefault true; # Thunar integration with plugins
     };
-
     programs.gdk-pixbuf.modulePackages = [
       pkgs.webp-pixbuf-loader
     ];
