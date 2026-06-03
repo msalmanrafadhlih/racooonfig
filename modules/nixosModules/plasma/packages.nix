dbc85b0ea89b570539b57c8cb8f72680aa6c2472
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-03 20:40:25 +0700

    2026-06-03 20:40 | Update via SAVEFLAKE

---
+++ modules/nixosModules/plasma/packages.nix
@@ -0,0 +1,30 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.racooonfig;
+in
+with pkgs; [
+  # Audio visualizer
+  dconf-editor
+
+  glib
+  gsettings-desktop-schemas
+  gsettings-qt
+
+] ++ (lib.mkIf (builtins.elem "macos-kdeplasma" cfg.listConfigurations) [
+  # Dependencies tema
+  sassc
+  rsync
+  libsForQt5.qtstyleplugin-kvantum # kvantum-qt5
+  kdePackages.qtstyleplugin-kvantum # versi Qt6
+
+
+  gnome-weather
+  gnome-maps
+  gnome-calendar
+  gnome-clocks
+])
