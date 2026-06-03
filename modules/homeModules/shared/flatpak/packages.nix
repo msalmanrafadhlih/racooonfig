be2f04a41c833d9172078a52517afb938e71d192
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-03 20:40:25 +0700

    Update via SAVEFLAKE - 2026-06-03 20:40
    Update file: modules/homeModules/shared/flatpak/packages.nix
    
    Changes:
    -  # Spotify
    +
    +

--- modules/homeModules/shared/flatpak/packages.nix
+++ modules/homeModules/shared/flatpak/packages.nix
@@ -1,6 +1,6 @@
 { config, lib }: let cfg = config.racooonfig;
 in [
-  # Spotify
+  # Spotify original
   (lib.mkIf (builtins.elem "spotify-flatpak" cfg.listConfigurations) {
     appId = "com.spotify.Client";
     origin = "flathub";
@@ -13,8 +13,34 @@ in [
   })
 
   # spotify Clint for Gnome desktop
+  # Note: Riff require premium account
   (lib.mkIf (builtins.elem "riff-flatpak" cfg.listConfigurations) {
     appId = "dev.diegovsky.Riff";
+    origin = "flathub";
+  })
+
+  # Whatsapp Client
+  (lib.mkIf (builtins.elem "whatsapp-flatpak" cfg.listConfigurations) {
+    appId = "com.rtosta.zapzap";
+    origin = "flathub";
+  })
+
+  # plasma addons
+  (lib.mkIf (builtins.elem "macos-kdeplasma-apps" cfg.listConfigurations) {
+    # Music player
+    appId = "io.bassi.Amberol";
+    origin = "flathub";
+  }{
+    # Dowload Manager
+    appId = "net.agalwood.Motrix";
+    origin = "flathub";
+  }{
+    # Listen to Ambient sound
+    appId = "com.rafaelmardojai.Blanket";
+    origin = "flathub";
+  }{
+    # Another Spotify Client
+    appId = "com.github.KRTirtho.Spotube";
     origin = "flathub";
   })
 ]
