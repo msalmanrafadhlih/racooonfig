62635704abe0cc8991ea408cfa60f6496b6fa990
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-08 18:55:05 +0700

    2026-06-08 18:55 | Update via SAVEFLAKE

---
+++ configs/beets/default.nix
@@ -0,0 +1,26 @@
+{
+  mkSymlink,
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+let
+  cfg = config.racooonfig;
+  configs = {
+    "beets/config.yaml" = "config";
+  };
+in
+
+{
+  config = lib.mkIf (cfg.homeManager && builtins.elem "beets" cfg.listConfigurations) {
+    home.packages = with pkgs; [
+      beets
+    ];
+
+    xdg.configFile = mkSymlink {
+      target = "beets";
+    } configs;
+  };
+}
