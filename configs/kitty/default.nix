9e25429ebb59ce23aac5f8af99ea76e6d5dbf03c
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-13 15:47:52 +0700

    Update via SAVEFLAKE - 2026-06-13 15:47
    Update file: configs/kitty/default.nix
    
    Changes:
    +
    -        # fonts
    -
    -
    -        window_alert_on_bell = "yes";
    -
    -        x11_hide_window_decorations = "yes";
    -        hide_window_decorations = "yes";
    -        draw_minimal_borders = "yes";
    -        enable_audio_bell = "yes";
    -        bell_on_tab = "🔔 ";
    -        remember_window_size = "no";
    -        allow_remote_control = "yes";
    -        listen_on = "unix:/tmp/kitty";
    -
    +
    +
    +
    +
    -        copy_on_select = "yes";
    -        cursor_trail = 1;
    -        cursor_trail_decay = "0.1 0.4";
    -        cursor_blink_interval = 1;
    -        scrollback_lines = 2000;
    -

--- configs/kitty/default.nix
+++ configs/kitty/default.nix
@@ -24,12 +24,20 @@ in
     };
 
     home.packages = with pkgs; [
+      # Perbaikan modul nerd-fonts untuk NixOS rilis terbaru (24.11+ / unstable)
       nerd-fonts.jetbrains-mono
       nerd-fonts.victor-mono
     ];
 
     programs.kitty = {
       enable = true;
+
+      # TWEAK UTAMA: Membungkus binary paket Kitty agar selalu dieksekusi dengan flag --single-instance
+      package = pkgs.kitty.overrideAttrs (oldAttrs: {
+        postInstall = (oldAttrs.postInstall or "") + ''
+          wrapProgram $out/bin/kitty --add-flags "--single-instance"
+        '';
+      });
 
       shellIntegration = {
         enableZshIntegration = true;
@@ -42,7 +50,7 @@ in
       '';
 
       settings = {
-        # fonts
+        # Fonts & Opacity
         bold_font = "JetBrainsMono NF";
         italic_font = "VictorMono Nerd Font";
         bold_italic_font = "VictorMono Nerd Font";
@@ -52,36 +60,43 @@ in
         placement_strategy = "center";
         confirm_os_window_close = 0;
         tab_bar_style = "powerline";
-
-
         window_padding_width = "10";
-        window_alert_on_bell = "yes";
         window_border_width = "3pt";
-
         inactive_text_alpha = "0.7";
 
-        x11_hide_window_decorations = "yes";
-        hide_window_decorations = "yes";
-        draw_minimal_borders = "yes";
-        enable_audio_bell = "yes";
-        bell_on_tab = "🔔 ";
-        remember_window_size = "no";
-        allow_remote_control = "yes";
-        listen_on = "unix:/tmp/kitty";
+        # OPTIMASI MEMORI RAM
+        scrollback_lines = 500; # Menghemat penggunaan buffer RAM
+        scrollback_pager_history_size = 0;
 
+        # OPTIMASI CPU & GRAFIS
+        repaint_delay = 30;     # Menurunkan FPS agar menghemat daya CPU
+        input_delay = 10;
+        sync_to_monitor = true;
+
+        # MENONAKTIFKAN ANIMASI YANG MEMAKAN RESOURCE
+        cursor_trail = 0;       # Mematikan jejak kursor (Sangat hemat resource GPU)
+        cursor_blink_interval = 0; # Kursor statis mencegah background render loop
+        enable_audio_bell = false;
+        window_alert_on_bell = false;
+        bell_on_tab = " ";
+
+        # PERBAIKAN TIPE DATA (Menggunakan Boolean asli, bukan String)
+        x11_hide_window_decorations = true;
+        hide_window_decorations = true;
+        draw_minimal_borders = true;
+        remember_window_size = false;
+        allow_remote_control = false; # Set false jika kontrol eksternal tidak digunakan
+        # listen_on = "unix:/tmp/kitty";
+
+        # Pengaturan Mouse & Seleksi teks
         mouse_hide_wait = 1;
         select_by_word_characters = "@-./_~?&=%+#a";
-        copy_on_select = "yes";
+        copy_on_select = true;
 
-        cursor_trail = 1;
-        cursor_trail_decay = "0.1 0.4";
         cursor_shape = "block";
         cursor_shape_unfocused = "beam";
         cursor_beam_thickness = 1.5;
         cursor_stop_blinking_after = 15.0;
-        cursor_blink_interval = 1;
-        scrollback_lines = 2000;
-
       };
     };
   };
