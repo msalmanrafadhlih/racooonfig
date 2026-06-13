4b95ac5af2eee869b471e699335ac0061b58a0f3
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-13 16:01:05 +0700

    Update via SAVEFLAKE - 2026-06-13 16:01
    Update file: modules/homeModules/plasma/packages.nix
    
    Changes:
    -
    -      # hiPrio memastikan wrapper ini menang atas qdbus dari qttools
    -      (lib.hiPrio (writeShellScriptBin "qdbus" ''
    -        exec ${kdePackages.qttools}/bin/qdbus "$@" 2>/dev/null || true
    -      ''))
    -      (lib.hiPrio (writeShellScriptBin "qdbus6" ''
    -        exec ${kdePackages.qttools}/bin/qdbus6 "$@" 2>/dev/null || true
    -      ''))
    -
    -      gnome-weather
    -      gnome-maps
    -      gnome-calendar
    -      gnome-clocks

--- modules/homeModules/plasma/packages.nix
+++ modules/homeModules/plasma/packages.nix
@@ -25,18 +25,5 @@ in
       kdePackages.qttools      # tool lain selain qdbus tetap tersedia
       application-title-bar
       kde-rounded-corners
-
-      # hiPrio memastikan wrapper ini menang atas qdbus dari qttools
-      (lib.hiPrio (writeShellScriptBin "qdbus" ''
-        exec ${kdePackages.qttools}/bin/qdbus "$@" 2>/dev/null || true
-      ''))
-      (lib.hiPrio (writeShellScriptBin "qdbus6" ''
-        exec ${kdePackages.qttools}/bin/qdbus6 "$@" 2>/dev/null || true
-      ''))
-
-      gnome-weather
-      gnome-maps
-      gnome-calendar
-      gnome-clocks
     ]);
 }
