13b61bcbb934e341eaa58dce296399ea52f5ecb7
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-06-13 15:12:55 +0700

    Update via SAVEFLAKE - 2026-06-13 15:12
    Update file: configs/kitty/default.nix
    
    Changes:
    -      # extraConfig = ''
    -      #   include themes/racooonfig.conf
    -      # '';
    +
    -        window_alert_on_bell = true;
    -        hide_window_decorations = true;
    -        draw_minimal_borders = true;
    -        enable_audio_bell = true;
    -        remember_window_size = false;
    -        copy_on_select = true;

--- configs/kitty/default.nix
+++ configs/kitty/default.nix
@@ -37,9 +37,9 @@ in
         mode = "no-title no-cwd";
       };
 
-      # extraConfig = ''
-      #   include themes/racooonfig.conf
-      # '';
+      extraConfig = ''
+        include themes/racooonfig.conf
+      '';
 
       settings = {
         # fonts
@@ -53,23 +53,25 @@ in
         confirm_os_window_close = 0;
         tab_bar_style = "powerline";
 
+
         window_padding_width = "10";
-        window_alert_on_bell = true;
+        window_alert_on_bell = "yes";
         window_border_width = "3pt";
 
         inactive_text_alpha = "0.7";
 
-        hide_window_decorations = true;
-        draw_minimal_borders = true;
-        enable_audio_bell = true;
+        x11_hide_window_decorations = "yes";
+        hide_window_decorations = "yes";
+        draw_minimal_borders = "yes";
+        enable_audio_bell = "yes";
         bell_on_tab = "🔔 ";
-        remember_window_size = false;
+        remember_window_size = "no";
         allow_remote_control = "yes";
         listen_on = "unix:/tmp/kitty";
 
         mouse_hide_wait = 1;
         select_by_word_characters = "@-./_~?&=%+#a";
-        copy_on_select = true;
+        copy_on_select = "yes";
 
         cursor_trail = 1;
         cursor_trail_decay = "0.1 0.4";
