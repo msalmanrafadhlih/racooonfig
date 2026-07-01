2cc4fdb3a7a8d58a2c745a58a7c47911bb8a96a8
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-01 22:13:21 +0700

    Update via SAVEFLAKE - 2026-07-01 22:13
    Update file: modules/homeModules/shared/scripts/cli/open-apps.nix
    
    Changes:
    -}: let
    +}:
    +
    +let
    -      browser="chromium --user-data-dir=$HOME/.config/Chromium"
    -          echo "Usage:"
    -          echo "  openapp --[app]"
    -          echo "  apps: gemini, claude, chatgpt"
    -          echo ""
    -          exit 1
    -          help
    -          --gemini)
    -              url="https://gemini.google.com"
    -              class="GeminiAi"
    -              echo "Opening $url..."
    -              exec $browser --app="$url" --class="$class"
    -              ;;
    -
    -          --claude)
    -              url="https://claude.ai"
    -              class="ClaudeAi"
    -              echo "Opening $url..."
    -              exec $browser --app="$url" --class="$class"
    -              ;;
    -
    -          --chatgpt)
    -              url="https://chat.openai.com"
    -              class="ChatAi"
    -              echo "Opening $url..."
    -              exec $browser --app="$url" --class="$class"
    -              ;;
    -          *)
    -              help
    -              ;;
    +
    +
    +
    -    home.packages = [ openapp ];

--- modules/homeModules/shared/scripts/cli/open-apps.nix
+++ modules/homeModules/shared/scripts/cli/open-apps.nix
@@ -3,57 +3,62 @@
   config,
   lib,
   ...
-}: let
+}:
+
+let
   openapp = pkgs.writeShellApplication {
     name = "openapp";
 
     text = ''
       set -euo pipefail
 
-      browser="chromium --user-data-dir=$HOME/.config/Chromium"
+      browser=(chromium --user-data-dir="$HOME/.config/Chromium")
 
       help() {
-          echo "Usage:"
-          echo "  openapp --[app]"
-          echo "  apps: gemini, claude, chatgpt"
-          echo ""
-          exit 1
+        echo "Usage:"
+        echo "  openapp [--comp]"
+        echo "  comp: gemini, claude, chatgpt"
+        exit 1
       }
 
       if [[ -z "''${1:-}" ]]; then
-          help
+        help
       fi
 
       case "$1" in
-          --gemini)
-              url="https://gemini.google.com"
-              class="GeminiAi"
-              echo "Opening $url..."
-              exec $browser --app="$url" --class="$class"
-              ;;
+        --gemini)
+          url="https://gemini.google.com"
+          class="GeminiAi"
+          echo "Opening $url..."
+          exec "''${browser[@]}" --app="$url" --class="$class"
+          ;;
 
-          --claude)
-              url="https://claude.ai"
-              class="ClaudeAi"
-              echo "Opening $url..."
-              exec $browser --app="$url" --class="$class"
-              ;;
+        --claude)
+          url="https://claude.ai"
+          class="ClaudeAi"
+          echo "Opening $url..."
+          exec "''${browser[@]}" --app="$url" --class="$class"
+          ;;
 
-          --chatgpt)
-              url="https://chat.openai.com"
-              class="ChatAi"
-              echo "Opening $url..."
-              exec $browser --app="$url" --class="$class"
-              ;;
-          *)
-              help
-              ;;
+        --chatgpt)
+          url="https://chat.openai.com"
+          class="ChatAi"
+          echo "Opening $url..."
+          exec "''${browser[@]}" --app="$url" --class="$class"
+          ;;
+
+        *)
+          help
+          ;;
       esac
     '';
   };
 in
 {
   config = lib.mkIf config.racooonfig.homeManager {
-    home.packages = [ openapp ];
+    home.packages = with pkgs; [
+      openapp
+      chromium
+    ];
   };
 }
