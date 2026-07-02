80fa91d16b80a1ae1683f182dbb0201e8f620064
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-03 04:00:09 +0700

    Update via SAVEFLAKE - 2026-07-03 04:00
    Update file: modules/nixosModules/default.nix
    
    Changes:
    -        export XCURSOR_PATH="/srv/share/icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
    -      "d /srv/share/wallpapers         2775 root users -"
    -      "d /srv/share/music              2775 root users -"
    -      "d /srv/share/files              2775 root users -"
    -      "d /srv/share/icons              2775 root users -"
    -      "d /srv/share/themes             2775 root users -"

--- modules/nixosModules/default.nix
+++ modules/nixosModules/default.nix
@@ -19,18 +19,18 @@ in
   config = lib.mkIf cfg.enable {
     environment = {
       extraInit = ''
-        export XCURSOR_PATH="/srv/share/icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
+        export XCURSOR_PATH="/srv/share/Icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
         export XDG_DATA_DIRS="/srv/share:/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
       '';
     };
 
     systemd.tmpfiles.rules = [
       "d /srv/share                    0755 root users -"
-      "d /srv/share/wallpapers         2775 root users -"
-      "d /srv/share/music              2775 root users -"
-      "d /srv/share/files              2775 root users -"
-      "d /srv/share/icons              2775 root users -"
-      "d /srv/share/themes             2775 root users -"
+      "d /srv/share/Wallpapers         2775 root users -"
+      "d /srv/share/Musics              2775 root users -"
+      "d /srv/share/Files              2775 root users -"
+      "d /srv/share/Icons              2775 root users -"
+      "d /srv/share/Themes             2775 root users -"
 
       "d /srv/share/fonts              2775 root users -"
       "d /srv/share/plasma             2775 root users -"
