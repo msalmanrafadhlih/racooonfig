cc067de1c8e456491fdbf69fcd1d4fc2c849050f
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-03 22:52:54 +0700

    Update via SAVEFLAKE - 2026-07-03 22:52
    Update file: modules/nixosModules/default.nix
    
    Changes:
    -       mapAll ./plasma   [ ] { }
    -    ++ mapAll ./bspwm    [ ] { }
    -    ++ mapAll ./niri     [ ] { }
    -    ++ mapAll ./shared   [ ] { };
    -        export XCURSOR_PATH="/srv/share/Icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
    -      "d /srv/share                    0755 root users -"
    -      "d /srv/share/Wallpapers         2775 root users -"
    -      "d /srv/share/Musics              2775 root users -"
    -      "d /srv/share/Files              2775 root users -"
    -      "d /srv/share/Icons              2775 root users -"
    -      "d /srv/share/Themes             2775 root users -"
    -
    -      "d /srv/share/fonts              2775 root users -"
    -      "d /srv/share/plasma             2775 root users -"
    -      "d /srv/share/plasma/plasmoids   2775 root users -"
    -      "d /srv/share/plasma/wallpapers  2775 root users -"
    -      "d /srv/share/kwin               2775 root users -"
    -      "d /srv/share/aurorae            2775 root users -"
    -      "d /srv/share/color-schemes      2775 root users -"
    -    ];

--- modules/nixosModules/default.nix
+++ modules/nixosModules/default.nix
@@ -10,35 +10,27 @@ let
 in
 {
   imports =
-       mapAll ./plasma   [ ] { }
+    mapAll ./plasma [ ] { }
     ++ mapAll ./hyprland [ ] { }
-    ++ mapAll ./bspwm    [ ] { }
-    ++ mapAll ./niri     [ ] { }
-    ++ mapAll ./shared   [ ] { };
+    ++ mapAll ./bspwm [ ] { }
+    ++ mapAll ./niri [ ] { }
+    ++ mapAll ./shared [ ] { };
 
   config = lib.mkIf cfg.enable {
     environment = {
       extraInit = ''
-        export XCURSOR_PATH="/srv/share/Icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
+        export XCURSOR_PATH="/srv/share/icons:/usr/share/icons''${XCURSOR_PATH:+:$XCURSOR_PATH}"
         export XDG_DATA_DIRS="/srv/share:/usr/share''${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
       '';
     };
 
     systemd.tmpfiles.rules = [
-      "d /srv/share                    0755 root users -"
-      "d /srv/share/Wallpapers         2775 root users -"
-      "d /srv/share/Musics              2775 root users -"
-      "d /srv/share/Files              2775 root users -"
-      "d /srv/share/Icons              2775 root users -"
-      "d /srv/share/Themes             2775 root users -"
-
-      "d /srv/share/fonts              2775 root users -"
-      "d /srv/share/plasma             2775 root users -"
-      "d /srv/share/plasma/plasmoids   2775 root users -"
-      "d /srv/share/plasma/wallpapers  2775 root users -"
-      "d /srv/share/kwin               2775 root users -"
-      "d /srv/share/aurorae            2775 root users -"
-      "d /srv/share/color-schemes      2775 root users -"
-    ];
+      "d /srv/share 3775 root users -"
+    ]
+    ++ (map (dir: "d /srv/share/${dir} 2775 root users -") [
+      "files" "fonts" "icons" "themes"
+      "plasma" "wallpapers" "kwin"
+      "aurorae" "color-schemes"
+    ]);
   };
 }
