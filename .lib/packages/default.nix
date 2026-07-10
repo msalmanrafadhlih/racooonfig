400352b3eb1f125fceac7ad6be40ca7ec55bac7d
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-10 19:54:16 +0700

    Update via SAVEFLAKE - 2026-07-10 19:54
    Update file: .lib/packages/default.nix
    
    Changes:
    -{ pkgs, ... }:
    +{ pkgs, ... }:
    -  desktopify-lite = pkgs.callPackage ./desktopify-lite.nix {};
    -  rip             = pkgs.callPackage ./process-manager.nix {};
    -  bloodrage-plymouth = pkgs.callPackage ./bloodrage-plymouth.nix {};
    -  qylock-sddm-theme  = pkgs.callPackage ./qylock-sddm-theme.nix {};
    -  my-grub-theme      = pkgs.callPackage ./my-grub-theme.nix {};
    -  cursor-memes       = pkgs.callPackage ./cursor-theme.nix {};

--- .lib/packages/default.nix
+++ .lib/packages/default.nix
@@ -1,16 +1,16 @@
 # ./default.nix
-{ pkgs, ... }: 
+{ pkgs, ... }:
 
 let
   # Definisikan paket individual agar bisa dipanggil secara mandiri
-  desktopify-lite = pkgs.callPackage ./desktopify-lite.nix {};
-  rip             = pkgs.callPackage ./process-manager.nix {};
+  desktopify-lite = pkgs.callPackage ./desktopify-lite.nix { };
+  rip = pkgs.callPackage ./process-manager.nix { };
 
   # overlay-packages
-  bloodrage-plymouth = pkgs.callPackage ./bloodrage-plymouth.nix {};
-  qylock-sddm-theme  = pkgs.callPackage ./qylock-sddm-theme.nix {};
-  my-grub-theme      = pkgs.callPackage ./my-grub-theme.nix {};
-  cursor-memes       = pkgs.callPackage ./cursor-theme.nix {};
+  bloodrage-plymouth = pkgs.callPackage ./bloodrage-plymouth.nix { };
+  qylock-sddm-theme = pkgs.callPackage ./qylock-sddm-theme.nix { };
+  my-grub-theme = pkgs.callPackage ./my-grub-theme.nix { };
+  cursor-memes = pkgs.callPackage ./cursor-theme.nix { };
 in
 {
   # Ekspos paket individual (for spesifik packages ex. 'nix build .#desktopify-lite')
@@ -23,6 +23,7 @@ in
     rip
     ;
 
+  # nix build .#all
   all = pkgs.symlinkJoin {
     name = "my-packages-collection";
     paths = [
