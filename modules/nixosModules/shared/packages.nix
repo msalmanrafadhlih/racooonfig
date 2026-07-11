fade3ada24b7998b8870ec73567aff99a267933a
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-07-12 03:33:46 +0700

    Update via SAVEFLAKE - 2026-07-12 03:33
    Update file: modules/nixosModules/shared/packages.nix
    
    Changes:
    -  inputs,
    -  system = pkgs.stdenv.hostPlatform.system;
    -  inp = inputs.racooonfig.inputs;
    -      inp.matugen.packages.${system}.default
    -

--- modules/nixosModules/shared/packages.nix
+++ modules/nixosModules/shared/packages.nix
@@ -1,21 +1,16 @@
 {
   pkgs,
-  inputs,
   lib,
   config,
   ...
 }:
 
 let
-  system = pkgs.stdenv.hostPlatform.system;
-  inp = inputs.racooonfig.inputs;
   cfg = config.racooonfig;
 in
 {
   config = lib.mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
-      inp.matugen.packages.${system}.default
-
       # High-performance Lua implementation
       luajit 
 
