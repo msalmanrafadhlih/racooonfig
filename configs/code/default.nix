ec59b8b9d6a1a9e6c9f2fd66dd38ed07c0aeb4af
msalmanrafadhlih <141149698+msalmanrafadhlih@users.noreply.github.com>
2026-09-20 12:48:02 +0700

    Update via SAVEFLAKE - 2026-09-20 12:48
    Update file: configs/code/default.nix
    
    Changes:
    deleted file mode 100644
    -{ config, lib, pkgs, ... }: let cfg = config.racooonfig;
    -in {
    -  config = lib.mkIf (cfg.homeManager && builtins.elem "vscode" cfg.listConfigurations) {
    -    home.packages = with pkgs; [
    -      vscode
    -    ];
    -  };
    -}

--- configs/code/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, ... }: let cfg = config.racooonfig;
-in {
-  config = lib.mkIf (cfg.homeManager && builtins.elem "vscode" cfg.listConfigurations) {
-    home.packages = with pkgs; [
-      vscode
-    ];
-  };
-}
